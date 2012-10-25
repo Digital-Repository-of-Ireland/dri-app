342c11e7eccb845ab07ad96f022682901d1fddbd
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-10-25 16:35:30 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NuigRnag::Application.config.secret_token = '5cc6f02b553b47dd7c0e7467e4c926ca10a3c462558741142efc3866271a895128d02950bc53d7c4956c7c71ae30f7ec05143d22d42ec48a5e6fe87c3afb0f27'
