e6096b93d349ceecdb83ceea7d66706ebfc6b1b4
Stuart Kenny <skenny@tchpc.tcd.ie>
2013-01-09 16:26:05 +0000

    Make a scenario for constructing objects pass, add launchy gem

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,3 +1,15 @@
+Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+
+When /^I attach the metadata file "(.*?)"$/ do |file|
+  attach_file("metadata_file", "spec/fixtures/#{file}")
+end
+
+Then /^I press "(.*?)"$/ do |button|
+  click_button(button)
+end
+
 Then /^(?:|I )should see "([^"]*)"$/ do |text|
   if page.respond_to? :should
     page.should have_content(text)
@@ -15,3 +27,18 @@ Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
     assert page.has_xpath?('//*', :text => regexp)
   end
 end
+
+Then /^show me the page$/ do
+  save_and_open_page
+end
+
+def path_to(page_name)
+    
+    case page_name
+  
+    when /new Digital Object page/
+      '/audios/new'
+
+    end
+end
+
