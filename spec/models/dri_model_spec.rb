b5e522038ad46008f50a9fdf7c92d0ed73091ca0
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2013-01-14 11:49:09 +0000

    fixing the list of required level 1 metadata attributes

--- spec/models/dri_model_spec.rb
+++ spec/models/dri_model_spec.rb
@@ -4,9 +4,8 @@ describe DRI::Model do
   it "should have an audio type with the level 1 required metadata fields" do
     t = DRI::Model::Audio.new
     t.title = nil
-    t.type = nil
     t.rights = nil
-    t.people = nil
+    t.language = nil
     t.should_not be_valid
   end
 end
