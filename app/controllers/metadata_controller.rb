245aa2a8aeb86b42bc049ee97f19bffc4490d672
Damien Gallagher <damien.gallagher@nuim.ie>
2013-01-17 11:52:50 +0000

    Workarounds for correct XML validation
    - The Local directory is changed to schema/ whenever the schema is loaded
      to work with Nokogiri's assumption that relative paths in xsi:import
      and xsi:include use the Rails local directory as the base.
    
    - To force all schemata to validate together, I extract their
      namespaces and locations and dynamically create a schema that imports
      and includes these schemata. It is this schema that is then validated
      by Nokogiri.
    
    I'm still not satisfied that we have to use workarounds to get validation
    working but these modifications will allow Dublin Core to validate in DRI.

--- app/controllers/metadata_controller.rb
+++ app/controllers/metadata_controller.rb
@@ -1,3 +1,4 @@
+
 class MetadataController < AssetsController
 
   # Renders the metadata XML stored in the descMetadata datastream.
@@ -89,34 +90,43 @@ class MetadataController < AssetsController
       if namespace.has_key?("xmlns:dc") &&
         namespace["xmlns:dc"].eql?("http://purl.org/dc/elements/1.1/")
 
-        # We have to extract the overall root schema from the XML Document as Dublin Core
-        # is designed to work with any custom container schema
-        schema_location = nil
+        # We have to extract all the schemata from the XML Document in order to validate correctly
+        schema_imports = []
 
+        # Firstly, if the root schema has no namespace, retrieve it from xsi:noNamespaceSchemaLocation
         if (@tmp_xml.root.namespace == nil)
-          schema_location = @tmp_xml.root.attr("xsi:noNamespaceSchemaLocation")
-        elsif (@tmp_xml.root.namespace.href != nil)
-          root_ns_href = @tmp_xml.root.namespace.href
-          schemata_by_ns = Hash[ @tmp_xml.root.attr("xsi:schemaLocation").scan(/(\S+)\s+(\S+)/) ]
-	  schema_location = schemata_by_ns[root_ns_href]
+          schema_imports = ["<xs:include schemaLocation=\""+@tmp_xml.root.attr("xsi:noNamespaceSchemaLocation")+"\"/>\n"]
+        end
+        
+        # Then, find all elments that have the "xsi:schemaLocation" attribute and retrieve their namespace and schemaLocation
+        @tmp_xml.xpath("//*[@xsi:schemaLocation]").each do |node|
+          schemata_by_ns = Hash[node.attr("xsi:schemaLocation").scan(/(\S+)\s+(\S+)/)]
+          schemata_by_ns.each do |ns,loc|
+	    schema_imports = schema_imports | ["<xs:import namespace=\""+ns+"\" schemaLocation=\""+loc+"\"/>\n"]
+          end
         end
 
-        if (schema_location == nil)
+        if (schema_imports.size == 0)
           flash[:notice] = "The XML file contains no schema to validate against."
         else
-          # Validation against schema that reference other schema, such as
-          # the qualifieddc schema seems to be broken in Nokogiri. So
-          # we are temporarily disabling it and allowing all incoming XML
-          # to pass validation.
-          #
-          # xsd = Nokogiri::XML::Schema(open(schema_location))
-          # validate_errors = xsd.validate(@tmp_xml)
-          validate_errors = nil
+          # Create a schema that imports and includes the schema used in the XML
+
+          all_schemata = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+  			"<xs:schema xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" elementFormDefault=\"qualified\">\n" +
+                        schema_imports.join("") + "</xs:schema>"
+
+	  # When parsing the schema, the local directory needs to point to the schema folder
+          # as a work around to the problem Nokogiri has with parsing relative path imports.
+          xsd = Dir.chdir(Rails.root.join('schema')) do |path|
+             Nokogiri::XML::Schema(all_schemata)
+          end
+
+          validate_errors = xsd.validate(@tmp_xml)
 
           if validate_errors == nil || validate_errors.size == 0
 	    result = true
           else
-            flash[:alert] = "Validation Errors: #{validate_errors.join(", ")}"
+            flash[:error] = "Validation Errors: #{validate_errors.join("\n ")}"
           end
         end
       else
