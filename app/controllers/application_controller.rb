238e6843dc28ac3cf0a7fa5da76fdc571833020a
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2013-01-21 16:57:31 +0000

    Added lib/*.rb to rdoc and fixed some duplication in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,14 @@
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller 
-   include Blacklight::Controller  
-# Adds Hydra behaviors into the application controller 
+  include Blacklight::Controller  
+
+  # Adds Hydra behaviors into the application controller 
   include Hydra::Controller::ControllerBehavior
 
   # Please be sure to impelement current_user and user_session. Blacklight depends on 
   # these methods in order to perform user specific actions. 
 
   layout 'blacklight'
-
-  # Adds a few additional behaviors into the application controller 
-   include Blacklight::Controller  
-# Adds Hydra behaviors into the application controller 
-  include Hydra::Controller::ControllerBehavior
 
   # Please be sure to impelement current_user and user_session. Blacklight depends on 
   # these methods in order to perform user specific actions. 
