97df170046c7c7d2db8f5807a5d472b39b870aac
Stuart Kenny <skenny@tchpc.tcd.ie>
2013-01-22 16:21:27 +0000

    Basic rdoc

--- lib/nuigrnag/id_service.rb
+++ lib/nuigrnag/id_service.rb
@@ -13,14 +13,25 @@
 # limitations under the License.
 
 module NuigRnag
+
+  # Generates a Nice Opaque Identifier (NOID).
+
   class IdService
+    # Template for the NOID. Generates a random 8 character number with 
+    # extended digits at chars 3,4 and 7,8, and a final computed check character
     @@minter = Noid::Minter.new(:template => '.reeddeeddk')
+
+    # Prefix used in the ID (defined in the application.rb configuration)
     @@namespace = NuigRnag::Application.config.id_namespace
+
+    # Tests if the indentifier is valid
     def self.valid?(identifier)
-      # remove the fedora namespace since it's not part of the noid
+      # remove the namespace since it's not part of the noid
       noid = identifier.split(":").last
       return @@minter.valid? noid
     end
+
+    # Generate the next NOID. Will cycle until an unused ID is found.
     def self.mint
       while true
         pid = self.next_id
@@ -28,11 +39,14 @@ module NuigRnag
       end
       return pid
     end
+
     protected
     def self.next_id
       # seed with process id so that if two processes are running they do not come up with the same id.
       @@minter.seed($$)
       return  "#{@@namespace}:#{@@minter.mint}"
     end
+
   end
+
 end
