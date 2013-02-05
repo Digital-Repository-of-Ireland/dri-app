901231bbebd048c49c28f7d3780daadffe1d2fa6
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2013-02-05 12:24:13 +0000

    Adding i18n support, preferred language is now stored in the user table.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,8 @@ class ApplicationController < ActionController::Base
 
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    if current_user
+      I18n.locale = current_user.locale || I18n.default_locale
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121219182229) do
+ActiveRecord::Schema.define(:version => 20130205105143) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",     :null => false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(:version => 20121219182229) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "guest",                  :default => false
+    t.string   "locale"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
