608016aaa2ec369f06f6f616d4df34efb1a7a6fc
Stuart Kenny <skenny@tchpc.tcd.ie>
2013-02-13 11:49:33 +0000

    Add flash notices

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,28 @@
 class RelationshipsController < AssetsController
 
   def create
-    object = ActiveFedora::Base.find(params[:id], {:cast => true})
-    collection = Collection.find(params[:collection_id])
 
-    collection.items << object
-    collection.save
+    @object = ActiveFedora::Base.find(params[:id], {:cast => true})
+    @collection = Collection.find(params[:collection_id])
 
-    redirect_to :controller => "collections", :action => "show", :id => params[:collection_id]  
+    @collection.items << @object
+
+    if @collection.save
+      respond_to do |format|
+        format.html { flash[:notice] = t('dri.flash.notice.added_to_collection')
+          redirect_to :controller => "collections", :action => "show", :id => params[:collection_id] }
+        format.json { render :json => @collection }
+      end
+    else
+      respond_to do |format|
+        format.html {
+          flash["alert"] = t('dri.flash.notice.not_added_to_collection') 
+          redirect_to :controller => "objects", :action => "edit", :id => params[:id]
+        }
+        format.json { render :json => @collection.errors}
+      end
+    end
+
   end
 
 end
