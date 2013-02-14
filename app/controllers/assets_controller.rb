01f91557732dbf22f0ec845a929dca53814c8f95
Raymond Noonan <raymond.noonan@nuim.ie>
2013-02-14 15:36:05 +0000

    Moved include for Blacklight::Catalog to AssetsController from ApplicationController to prevent Devise routing error

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -4,6 +4,8 @@ class AssetsController < ApplicationController
   include Hydra::AccessControlsEnforcement
   include DRI::Metadata
   include DRI::Model
+  #Moved from application controller due to routing issues with devise
+  include Blacklight::Catalog
 
   # Retrieves a Fedora Digital Object by ID
   def retrieve_object(id)
