345703dff2fcee6938b2862f4fd77dbbf5b37abe
Stuart Kenny <skenny@tchpc.tcd.ie>
2013-02-25 10:49:26 +0000

    New collection control and tweet button

--- app/assets/javascripts/dri.js
+++ app/assets/javascripts/dri.js
@@ -52,6 +52,28 @@ $(document).ready(function() {
     });  
 });
 
+//= require blacklight/core
+//= require blacklight/checkbox_submit
+(function($) {
+//change form submit toggle to checkbox
+    Blacklight.do_collection_toggle_behavior = function() {
+      $(Blacklight.do_collection_toggle_behavior.selector).bl_checkbox_submit({
+          checked_label: "In Collection",
+          unchecked_label: "Add to Collection",
+          progress_label: "Saving...",
+          //css_class is added to elements added, plus used for id base
+          css_class: "toggle_collection"
+      });
+    };
+    Blacklight.do_collection_toggle_behavior.selector = "form.collection_toggle";
+
+$(document).ready(function() {
+  Blacklight.do_collection_toggle_behavior();
+});
+
+
+})(jQuery);
+
 //uploader_submit = function() {
 //  $('.uploader_submit').click(function() {
 //     $('#file_uploader').submit();
