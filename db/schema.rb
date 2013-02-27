2bad732943bd7984cc087149def4bebc232ac6eb
Raymond Noonan <raymond.noonan@nuim.ie>
2013-02-27 15:52:19 +0000

    Updated schema to reflect using usergroup engine

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130205105143) do
+ActiveRecord::Schema.define(:version => 20130220142124) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",     :null => false
@@ -40,7 +40,27 @@ ActiveRecord::Schema.define(:version => 20130205105143) do
 
   add_index "searches", ["user_id"], :name => "index_searches_on_user_id"
 
-  create_table "users", :force => true do |t|
+  create_table "user_group_groups", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
+    t.boolean  "is_locked",   :default => false
+  end
+
+  add_index "user_group_groups", ["name"], :name => "index_groups_on_name", :unique => true
+
+  create_table "user_group_memberships", :force => true do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.integer  "approved_by"
+  end
+
+  add_index "user_group_memberships", ["group_id", "user_id"], :name => "index_memberships_on_group_id_and_user_id", :unique => true
+
+  create_table "user_group_users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
     t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
@@ -54,10 +74,12 @@ ActiveRecord::Schema.define(:version => 20130205105143) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "guest",                  :default => false
+    t.string   "first_name"
+    t.string   "second_name"
     t.string   "locale"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "user_group_users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "user_group_users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
