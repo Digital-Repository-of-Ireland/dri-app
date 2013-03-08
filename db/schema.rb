b206cbf3171dab8819a7d31e2554923039513e3c
Raymond Noonan <raymond.noonan@nuim.ie>
2013-03-08 11:34:25 +0000

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130305154630) do
+ActiveRecord::Schema.define(:version => 20130308110258) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",     :null => false
@@ -74,10 +74,10 @@ ActiveRecord::Schema.define(:version => 20130305154630) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
-    t.boolean  "guest",                  :default => false
     t.string   "first_name"
     t.string   "second_name"
     t.string   "locale"
+    t.boolean  "guest",                  :default => false
   end
 
   add_index "user_group_users", ["email"], :name => "index_users_on_email", :unique => true
