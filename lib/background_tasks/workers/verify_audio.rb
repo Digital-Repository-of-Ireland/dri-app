3a093da5f83200f53526a81423ed3ff2bc25bdd7
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2013-04-19 16:29:30 +0100

    Debugging workers and removing duplicate validation code (note, same function now called at ingest and as bg task to validate file type, maybe we don't need it at ingest now)

--- lib/background_tasks/workers/verify_audio.rb
+++ lib/background_tasks/workers/verify_audio.rb
@@ -3,5 +3,18 @@ class VerifyAudio
 
   def self.perform(object_id)
     puts "Verifying that the file for #{object_id} is a valid audio file"
+
+    datastream = "masterContent"
+    @object = ActiveFedora::Base.find(object_id,{:cast => true})
+    @local_file_info = LocalFile.find(:all, :conditions => [ "fedora_id LIKE :f AND ds_id LIKE :d",
+                                                             { :f => @object.id, :d => datastream } ],
+                                      :order => "version DESC",
+                                      :limit => 1)
+    filename = @local_file_info.first.path
+
+    unless Validators.valid_file_type?(filename, @object.whitelist_type, @object.whitelist_subtypes)
+      puts "Found a problem"
+    end
+
   end
 end
