b06f52aaa16714055462259bb759ba801e8de2f6
Raymond Noonan <raymond.noonan@nuim.ie>
2013-04-19 15:16:28 +0100

    Updated spec helper to use poltergeist

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'simplecov'
 require 'simplecov-rcov'
-require 'capybara-webkit'
+require 'capybara/poltergeist'
 
-Capybara.javascript_driver = :webkit
+Capybara.javascript_driver = :poltergeist
 
 SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.start
