0ac8ca09f86be00bd5085aed01254a99b2845540
Raymond Noonan <raymond.noonan@nuim.ie>
2013-05-07 14:53:27 +0100

    Updated access checking

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -9,14 +9,31 @@ class AssetsController < ApplicationController
 
   # Retrieves a Fedora Digital Object by ID
   def retrieve_object(id)
-    enforce_edit_permissions!
+    enforce_permissions!
     return objs = ActiveFedora::Base.find(id,{:cast => true})
   end
 
   private
-  def enforce_edit_permissions!
-    unless can? :edit, params[:id]
-        raise Hydra::AccessDenied.new(t('dri.flash.alert.edit_permission'), :edit, params[:id])
+  #This might have to be moved/updated. Currently only checking upon retrieve object,
+  #might want to change so each controller specifys when to check & what methods to check.
+  def enforce_permissions!
+    debugger
+    action = params[:action]
+    if action.nil? or action=="edit" or action=="update"
+      unless can? :edit, params[:id]
+          raise Hydra::AccessDenied.new(t('dri.flash.alert.edit_permission'), :edit, params[:id])
+      end
+    elsif action=="show"
+      unless can? :read ,params[:id]
+        raise Hydra::AccessDenied.new(t('dri.flash.alert.read_permission'), :read, params[:id])
+      end
+    elsif action=="create"
+      #NOTE: create given to all users in registered by default
+      unless can? :create ,params[:id]
+        raise Hydra::AccessDenied.new(t('dri.flash.alert.create_permission'), :create, params[:id])
+      end
+    else
+      raise Hydra::AccessDenied.new(t('Unsupported Permission Level'), :read, params[:id])
     end
   end
   
