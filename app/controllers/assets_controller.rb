55644498fb7ea52b41d67faf92e3ac64a267238e
Raymond Noonan <raymond.noonan@nuim.ie>
2013-06-07 16:26:33 +0100

    Remove permissions check from retrieve_object

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -12,7 +12,6 @@ class AssetsController < CatalogController
 
   # Retrieves a Fedora Digital Object by ID
   def retrieve_object(id)
-    enforce_permissions!
     return objs = ActiveFedora::Base.find(id,{:cast => true})
   end
 
@@ -34,21 +33,4 @@ class AssetsController < CatalogController
     end
   end
 
-  #This might have to be moved/updated. Currently only checking upon retrieve object,
-  #might want to change so each controller specifys when to check & what methods to check.
-  def enforce_permissions!
-    action = params[:action]
-    if action.nil? or action=="edit" or action=="update" or action=="create"
-      unless can? :edit, params[:id]
-          raise Hydra::AccessDenied.new(t('dri.flash.alert.edit_permission'), :edit, params[:id])
-      end
-    elsif action=="show"
-      unless can? :read ,params[:id]
-        raise Hydra::AccessDenied.new(t('dri.flash.alert.read_permission'), :read, params[:id])
-      end
-    else
-      raise Hydra::AccessDenied.new(t('dri.flash.alert.unknown_permission', :action => action), :read, params[:id])
-    end
-  end
-  
 end
