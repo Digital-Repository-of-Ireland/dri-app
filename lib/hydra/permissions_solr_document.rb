a778d8f97d29191645d6d3b94e0fa7f6c369a1fd
Raymond Noonan <raymond.noonan@nuim.ie>
2013-06-07 16:24:47 +0100

    Added new methods to solr document which are used when evaluation permissions

--- lib/hydra/permissions_solr_document.rb
+++ lib/hydra/permissions_solr_document.rb
@@ -10,10 +10,33 @@ class Hydra::PermissionsSolrDocument < SolrDocument
   end
 
   def is_public?
-    access_key = ActiveFedora::SolrService.solr_name("access", Hydra::Datastream::RightsMetadata.indexer)
-    self[access_key].present? && self[access_key].first.downcase == "public"
+    key = ActiveFedora::SolrService.solr_name("access", Hydra::Datastream::RightsMetadata.indexer)
+    self[key].present? && self[key].first.downcase == "public"
+  end
+
+  def is_private?
+    key = ActiveFedora::SolrService.solr_name('private_metadata', Hydra::Datastream::RightsMetadata.integer_indexer).to_s
+    if self[key].present?
+      return true if self[key].to_s == "1"
+      return false if self[key].to_s == "0"
+    end
+    return nil
+  end
+
+  def show_master_file?
+    key = ActiveFedora::SolrService.solr_name('master_file', Hydra::Datastream::RightsMetadata.integer_indexer).to_s
+    if self[key].present?
+      return true if self[key].to_s == "1"
+      return false if self[key].to_s == "0"
+    end
+    return nil
+  end
+
+  def is_published?
+    #TODO:: finish
+    key = "properties_status_ssm" #ActiveFedora::SolrService.solr_name('properties_status', Hydra::Datastream::RightsMetadata).to_s
+    self[key].present? && self[key].first.downcase == "published"
   end
   
 
 end
-
