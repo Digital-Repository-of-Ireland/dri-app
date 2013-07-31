9d2db68595cdb394176015d99024bd4f9648344e
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2013-07-31 13:36:00 +0000

    Changes to get authentication for user_group pages to work.

--- config/routes.rb
+++ config/routes.rb
@@ -34,9 +34,9 @@ NuigRnag::Application.routes.draw do
   match 'objects/:id' => 'catalog#show', :via => :get
 
   # need to put in the 'system administrator' role here
-  #authenticate :admin do
+  authenticate do
     mount Resque::Server, :at => "/resque"
-  #end
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
