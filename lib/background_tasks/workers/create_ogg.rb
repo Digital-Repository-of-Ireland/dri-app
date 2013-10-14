8c04514c6a4a93cd93ae6a7426bc476a16d3cbdb
Stuart Kenny <skenny@tchpc.tcd.ie>
2013-10-14 15:05:14 +0100

    Clean up temporary files created by workers and encapsulate common code

--- lib/background_tasks/workers/create_ogg.rb
+++ lib/background_tasks/workers/create_ogg.rb
@@ -1,43 +1,9 @@
-class CreateOgg
+class CreateOgg < CreateAudio
   @queue = "create_ogg_queue"
+  @type = "ogg"
 
   require 'open3'
   require 'storage/s3_interface'
-
-  def self.perform(object_id)
-    Rails.logger.info "Creating Ogg version of #{object_id} asset"
-
-    datastream = "masterContent"
-    @object = ActiveFedora::Base.find(object_id,{:cast => true})
-    @local_file_info = LocalFile.find(:all, :conditions => [ "fedora_id LIKE :f AND ds_id LIKE :d",
-                                                             { :f => @object.id, :d => datastream } ],
-                                      :order => "version DESC",
-                                      :limit => 1)
-    masterfile = @local_file_info.first.path
-
-    tmp_dir = File.join(Dir::tmpdir, "dri_ogg_#{Time.now.to_i}_#{rand(100)}")
-    Dir.mkdir(tmp_dir)
-    workingfile = File.join(tmp_dir, File.basename(masterfile))
-    FileUtils.cp(masterfile,workingfile)
-    outputfile = File.join(tmp_dir, "output_file.ogg")
-
-    begin
-      transcode(workingfile, output_options, outputfile)
-    rescue Exceptions::BadCommand => e
-      Rails.logger.error "Failed to transcode file #{e.message}"
-      # requeue?
-    end
-
-    filename = "#{object_id}_ogg_web_quality.ogg"
-    Storage::S3Interface.store_surrogate(object_id, outputfile, filename)
-
-  end
-
-
-  def self.executable
-    Settings.plugins.ffmpeg_path
-  end
-
 
   # Specify the options for the ogg output file
   # codec, channel, bitrate, frequency, strip artwork?, strip tags?
@@ -51,19 +17,5 @@ class CreateOgg
     strip_metadata = "-map_metadata -1" if Settings.ogg_web_quality_out_options.strip_metadata.eql?("yes")
     "#{codec} -vn #{channel} #{bitrate} #{frequency} #{strip_metadata}"
   end
-
-
-  # Transcode the file
-  def self.transcode(input_file, options, output_file)
-    command = "#{executable} -y -i #{input_file} #{options} #{output_file}"
-    stdin, stdout, stderr, wait_thr = Open3::popen3(command)
-    stdin.close
-    out = stdout.read
-    stdout.close
-    err = stderr.read
-    stderr.close
-    raise Exceptions::BadCommand.new "Unable to execute command \"#{command}\"\n#{err}" unless wait_thr.value.success?
-  end
-
 
 end
