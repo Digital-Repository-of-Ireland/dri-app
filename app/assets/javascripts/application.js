1fec160f6b405e4d04f3b235a7806fca76388db3
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2013-11-27 12:34:46 +0000

    Removing the facebook javascript. This is disabled for the pilot and should be regenerated if it is ever added back in. Should get rid of annoying FB errors.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,14 +21,6 @@
 //
 //= require dri
 
-$(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
 $(document).ready(function() {
