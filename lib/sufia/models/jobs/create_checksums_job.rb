4b4d466ee0c9249d0123899798a65ee0dfb33f47
Damien Gallagher <damien.gallagher@nuim.ie>
2013-11-27 17:23:01 +0000

    CreateChecksumsJob is now working (requires a bundle update)
    Changed references to 'pid' to 'generic_file_id' in our resque jobs to make
    it clear that these resque jobs are being run on GenericFile ids, not Batch
    ids.

--- lib/sufia/models/jobs/create_checksums_job.rb
+++ lib/sufia/models/jobs/create_checksums_job.rb
@@ -7,20 +7,19 @@ class CreateChecksumsJob < ActiveFedoraPidBasedJob
   end
 
   def run
-    Rails.logger.info "Creating checksums of #{pid} asset"
+    Rails.logger.info "Creating checksums of #{generic_file_id} asset"
 
-    datastream = "content"
-    @local_file_info = LocalFile.find(:all, :conditions => [ "fedora_id LIKE :f AND ds_id LIKE :d",
-                                                             { :f => pid, :d => datastream } ],
+    @local_file_info = LocalFile.find(:all, :conditions => [ "fedora_id LIKE :f AND ds_id LIKE 'content'",
+                                                             { :f => generic_file_id } ],
                                       :order => "version DESC",
                                       :limit => 1)
     filename = @local_file_info.first.path
 
     # If the checksum is already set should we be comparing them?
     # What happens when the asset is updated then?
-    object.resource_md5 = Checksum.md5(filename) 
-    object.resource_sha256 = Checksum.sha256(filename) 
-    object.resource_rmd160 = Checksum.rmd160(filename)
+    object.checksum_md5 = Checksum.md5(filename) 
+    object.checksum_sha256 = Checksum.sha256(filename) 
+    object.checksum_rmd160 = Checksum.rmd160(filename)
     object.save
   end
 
