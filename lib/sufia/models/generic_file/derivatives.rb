36b6ac7e8356078b276e89d38462a9d0f012544d
Eoin Kilfeather <ekilfeather@gmail.com>
2014-02-19 08:47:48 +0000

    same thumbnail jobs for PDF as Image type"

--- lib/sufia/models/generic_file/derivatives.rb
+++ lib/sufia/models/generic_file/derivatives.rb
@@ -10,9 +10,15 @@ module Sufia
           case obj.mime_type
           when *pdf_mime_types
             obj.transform_datastream :content,
-              { :large => {size: "400", datastream: 'thumbnail_large'},
-              	:lightbox => {size: "600", datastream: 'lightbox_format'}
-               }
+       		  { 
+                :small => {size: "75", datastream: 'thumbnail_small'},
+                :medium => {size: "200", datastream: 'thumbnail_medium'},
+                :large => {size: "400", datastream: 'thumbnail_large'},
+                :lightbox => {size: "600", datastream: 'lightbox_format'},
+                :full => {size: "100%", datastream: 'full_size_web_format'},
+                :crop16_9_width_200 => {size: "200", crop: "200x113+0+0", gravity: "Center", datastream: 'crop16_9_width_200_thumbnail'},
+                :crop16_9_width_2228 => {size: "228", crop: "228x127+0+0", gravity: "Center", datastream: 'crop16_9_width_228_thumbnail'} 
+              }          
           when *audio_mime_types
             obj.transform_datastream :content,
               { :mp3 => {format: 'mp3', datastream: 'mp3'},
