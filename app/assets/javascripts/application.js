f0b51e8ede2f18cb61850668f795148181476b70
ekilfeather <ekilfeather@gmail.com>
2014-04-09 14:12:20 +0100

    issue694 - banner fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,8 @@
 
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
-<!-- Dropit Script - uses JQuery-UI Drop function -->
+//* Dropit Script - uses JQuery-UI Drop function
+
 $(document).ready(function() {
     $('.menu').dropit();
     $('.carousel').carousel();
