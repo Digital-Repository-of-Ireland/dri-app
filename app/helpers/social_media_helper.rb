c921c3371de820e2af024e0cb049c7728bcc358e
Stuart Kenny <skenny@tchpc.tcd.ie>
2014-05-02 16:58:11 +0100

    Fix for bug #885, make sure feed has entries

--- app/helpers/social_media_helper.rb
+++ app/helpers/social_media_helper.rb
@@ -2,7 +2,8 @@ module SocialMediaHelper
   def fetch_rss_feed(urls, channel)
     feeds = Feedjira::Feed.fetch_and_parse urls
     feed = feeds[channel]
-    return feed.entries
+
+    return feed == 0 ? {} : feed.entries
   end
 
 end
