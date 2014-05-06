9bc5d76b5824499ed97e8951aed65a24873da678
Fabrizio Covone <fabrizio.valerio.covone@gmail.com>
2014-05-06 18:07:23 +0100

    issue771 number per page selector alligned with design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,6 +45,13 @@ $(function(){
         }
         return false;
     });
+    $('#dri_page_options_id').bind('change', function () {
+        var url = $(this).val(); // get selected value
+        if (url) { // require a URL
+            window.location = url; // redirect
+        }
+        return false;
+    });
   });
   
 
