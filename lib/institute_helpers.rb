d8438b909b7eb4eebb9d1b5e7db1218756f9c710
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2014-06-05 16:54:59 +0100

    Issue #705 adding a way to identify the depositing institute.

--- lib/institute_helpers.rb
+++ lib/institute_helpers.rb
@@ -15,12 +15,36 @@ module InstituteHelpers
   end
 
 
+  def self.get_depositing_institute(collection)
+    return nil if collection.depositing_institute.blank?
+    Institute.where(:name => collection.depositing_institute).first
+  end
+
   def self.get_institutes_from_solr_doc(doc)
     doc['object_type_ssm'][0] == 'Collection' ? self.get_collection_institutes_from_solr_doc(doc) : self.get_object_institutes_from_solr_doc(doc)
   end
 
 
-  def self.get_object_institutes_from_solr_doc(doc)
+  def self.get_depositing_institute_from_solr_doc(doc)
+    doc['object_type_ssm'][0] == 'Collection' ? self.get_collection_depositing_institute_from_solr_doc(doc) : self.get_object_depositing_institute_from_solr_doc(doc)
+  end
+
+
+  def self.get_collection_depositing_institute_from_solr_doc(doc)
+    return Institute.where(:name => doc['depositing_institute_ssm']).first unless doc['depositing_institute_ssm'].blank?
+    return nil
+  end
+
+
+  def self.get_object_depositing_institute_from_solr_doc(doc)
+    id = doc['is_governed_by_ssim'][0].gsub(/^info:fedora\//, '')
+    solr_query = "id:#{id}"
+    collection = ActiveFedora::SolrService.query(solr_query, :defType => "edismax", :rows => "1", :fl => "id,depositing_institute_ssm").first
+    return Institute.where(:name => collection['depositing_institute_ssm']).first unless collection['depositing_institute_ssm'].blank?
+    return nil
+  end
+
+  def self.get_object_institutes_from_solr_doc(doc, depositing=nil)
     allinstitutes = Institute.find(:all)
     myinstitutes = []
     id = doc['is_governed_by_ssim'][0].gsub(/^info:fedora\//, '')
