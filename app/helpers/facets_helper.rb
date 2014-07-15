d12308e93b3323c8e8276a03c5454bfbff0c5ea2
Fabrizio Covone <fabrizio.valerio.covone@gmail.com>
2014-07-15 17:59:32 +0100

    issue 1025 facet folding fixed and updated to the new structure introduced by blacklight 5

--- app/helpers/facets_helper.rb
+++ app/helpers/facets_helper.rb
@@ -191,12 +191,26 @@ module FacetsHelper
   end
 
 
+  ##
+  # Standard display of a facet value in a list. Used in both _facets sidebar
+  # partial and catalog/facet expanded list. Will output facet value name as
+  # a link to add that to your restrictions, with count in parens.
+  #
+  # @param [Blacklight::SolrResponse::Facets::FacetField]
+  # @param [String] facet item
+  # @param [Hash] options
+  # @option options [Boolean] :suppress_link display the facet, but don't link to it
+  # @return [String]
+  def render_facet_value(facet_solr_field, item, options ={})
+    path = search_action_path(add_facet_params_and_redirect(facet_solr_field, item))
+    link_to_unless(options[:suppress_link], facet_display_value(facet_solr_field, item) + " (#{item.hits})", path, :class=>"facet_select")
+  end
+
   # Renders a count value for facet limits. Can be over-ridden locally
   # to change style. And can be called by plugins to get consistent display
   def render_facet_count(num)
     content_tag("b", t('blacklight.search.facets.count', :number => num))
   end
-
 
   # Standard display of a SELECTED facet value, no link, special span
   # with class, and 'remove' button.
