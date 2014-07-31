ba6aa62fe5632ce034f3504632f15b77eebbac25
Fabrizio Covone <fabrizio.valerio.covone@gmail.com>
2014-07-31 16:11:45 +0100

    issue 1070 refined query to get only valid objects collections that have a creation_date, date parsing tested for iso and w3c standards, dcmi date parser developed

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,8 +1,46 @@
 class TimelineController < ApplicationController
 
   def get
-    query = ""
+
     timeline_data = { :timeline => {} }
+
+    query = get_query(params)
+
+    puts "###################"
+    puts query
+    puts "###################"
+
+    response = ActiveFedora::SolrService.query(query, :defType => "edismax", :rows => "150")
+
+
+    puts "±±±±±±±±±±±±±±±±±±"
+    puts response.inspect
+    puts "±±±±±±±±±±±±±±±±±±"
+
+    ######## TO BE REMOVED BEGIN
+    iso_date = "1986-02-18"
+    w3c_date = "Wed, 09 Feb 1994"
+    dcmi_date = "name=Phanerozoic Eon; end=2009-09-25T16:40+10:00; start=1999-09-25T14:20+10:00; scheme=W3C-DTF;"
+    iso_conv_date = Date.parse(iso_date)
+    w3c_conv_date = Date.parse(w3c_date)
+    parsed_dcmi = parse_dcmi(dcmi_date)
+    puts parse_dcmi?(w3c_date).to_s
+    puts iso_conv_date.inspect
+    puts w3c_conv_date.inspect
+    puts parse_dcmi?(dcmi_date).to_s
+    puts parsed_dcmi.inspect
+    ######## TO BE REMOVED END
+
+    response.each do |document|
+      document = document.symbolize_keys
+      puts document[Solrizer.solr_name('creation_date', :stored_searchable, type: :string).to_sym].first
+    end
+    render :json => timeline_data.to_json
+  end
+
+  private
+  def get_query params
+    query = ""
 
     if params[:mode] == 'collections'
       query += "#{Solrizer.solr_name('file_type', :stored_searchable, type: :string)}:collection"
@@ -24,19 +62,32 @@ class TimelineController < ApplicationController
       end
     end
 
+    query += " AND #{Solrizer.solr_name('creation_date', :stored_searchable, type: :string)}:[* TO *]"
     query += " AND (-#{Solrizer.solr_name('creation_date', :stored_searchable, type: :string)}:unknown"
     query += " AND -#{Solrizer.solr_name('creation_date', :stored_searchable, type: :string)}:\"n/a\")"
 
-    puts "###################"
-    puts query
-    puts "###################"
+    return query
+  end
 
-    response = ActiveFedora::SolrService.query(query, :defType => "edismax", :rows => "3")
+  def parse_dcmi dcmi_date
+    parsed_dcmi = {}
+    return 'nil' if dcmi_date.nil?
 
-    puts "±±±±±±±±±±±±±±±±±±"
-    puts response.inspect
-    puts "±±±±±±±±±±±±±±±±±±"
+    dcmi_date.split(/\s*;\s*/).each do |component|
+      (k,v) = component.split(/\s*=\s*/)
+      if (k == 'start' || k == 'end')
+        parsed_dcmi[k.to_sym] = Date.parse(v)
+      end
+    end
 
-    render :json => timeline_data.to_json
+    if parsed_dcmi.blank?
+      return dcmi_date
+    end
+
+    return parsed_dcmi
+  end
+
+  def parse_dcmi? dcmi_date
+    return dcmi_date != parse_dcmi(dcmi_date)
   end
 end
