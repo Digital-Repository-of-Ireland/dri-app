941e29d77ed7cd8a362f0e7c9c360535e8cb9cb0
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2014-08-15 12:02:50 +0100

    Fleshing out the object history report

--- app/controllers/object_history_controller.rb
+++ app/controllers/object_history_controller.rb
@@ -1,8 +1,9 @@
 # Controller for generating a per-object history/audit report
 #
-
+require 'inheritance_methods'
 
 class ObjectHistoryController < ApplicationController
+  include InheritanceMethods
 
   before_filter :authenticate_user_from_token!, :only => [:show]
   before_filter :authenticate_user!, :only => [:show]
@@ -24,6 +25,14 @@ class ObjectHistoryController < ApplicationController
     end
 
     @datastreams = @records.keys
+
+    # Get inherited values
+    @institute_manager = get_institute_manager(@object)
+    @read_groups = get_governing_attribute(@object, 'read_groups_string')
+    @read_users = get_read_users_via_group(@object)
+    @edit_users = get_governing_attribute(@object, 'edit_users_string')
+    @manager_users = get_governing_attribute(@object, 'manager_users_string')
+    @licence = get_governing_attribute(@object, 'licence').to_s
 
     respond_to do |format|
       format.html
