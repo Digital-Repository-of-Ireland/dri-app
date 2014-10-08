8301de098d5cf1b3dd175993115955f96724febf
Eoin Kilfeather <ekilfeather@gmail.com>
2014-10-08 16:42:49 +0100

    issue1108 - reverted hidden collection field, more js and styling

--- app/assets/javascripts/dri.js
+++ app/assets/javascripts/dri.js
@@ -12,55 +12,67 @@ $(document).ready(function() {
 // Whenever an "add" link is clicked, a new text field is added to the bottom of the list 
 add_text_field = function() {
     $('.add-text-field a').click(function() {
-      var fieldset_name = $(this).parent().parent().attr('id');
+      var fieldset_name = $(this).parents('fieldset').attr('id');
       var model_name = $(this).attr('model-name');
-      $(this).parent().before(  
-      '<input class="edit span6 dri-textfield" id="'+model_name+'_'+fieldset_name+'][" name="'+model_name+'['+fieldset_name+'][]" size="30" type="text" value=""> <a class="destructive">&nbsp;<i class="fa fa-times-circle"></i> Remove</a>');
-      $(this).parent().siblings('a').last().click(function() {
-        $(this).prev('input[type="text"]').remove();
-        $(this).prev('br').remove();
-        $(this).remove();
-        return false;
-      });
+      if (fieldset_name == 'description'  || fieldset_name == 'rights') {
+	      $(this).parent().before(  
+	      '<textarea class="edit span6 dri-textarea" id="'+model_name+'_'+fieldset_name+'][" name="'+model_name+'['+fieldset_name+'][]"></textarea> <a class="destructive" model-name="batch">&nbsp;<i class="fa fa-times-circle"></i> Remove</a>');
+	      $(this).parent().siblings('a').last().click(function(){
+	        $(this).prev('textarea').remove();
+	        $(this).remove();
+	        return false;
+	      });
+      } else {
+	      $(this).parent().before(  
+	      '<input class="edit span6 dri-textfield" id="'+model_name+'_'+fieldset_name+'][" name="'+model_name+'['+fieldset_name+'][]" size="30" type="text" value=""> <a class="destructive">&nbsp;<i class="fa fa-times-circle"></i> Remove</a>');
+	      $(this).parent().siblings('a').last().click(function() {
+
+	        $(this).prev('input[type="text"]').remove();
+	        $(this).remove();
+	        return false;
+	      });
+      }
       return false;
     });
-  }
+  };
 
 // Remove the text field and the "delete" button whenever the "delete" link is clicked
 destroy_text_field = function() {
     $('.destructive').click(function() {
-      var fieldset_name = $(this).parent().attr('id');
+      var fieldset_name = $(this).parents('fieldset').attr('id');
       var model_name = $(this).attr('model-name');
-
-      if ((fieldset_name == 'roles') && $(this).siblings('select').length > 1) {
-        
+      
+      if (fieldset_name == 'description'  || fieldset_name == 'rights') {
+        $(this).prev('textarea').remove();
+        $(this).before('<input type="hidden" id="'+model_name+'_'+fieldset_name+'][" name="'+model_name+'['+fieldset_name+'][]" value="">');
+        $(this).remove();
+      }
+      else if ((fieldset_name == 'roles') && $(this).siblings('select').length > 1) {
         $(this).prev('input[type="text"]').remove();
         var selected_value = $(this).prev('select').val();
         $(this).prev('select').remove();
-        $(this).prev('br').remove();
         $(this).before('<input type="hidden" id="'+model_name+'_'+fieldset_name+'][type][" name="'+model_name+'['+fieldset_name+'][type][]" value="'+selected_value+'">');
         $(this).before('<input type="hidden" id="'+model_name+'_'+fieldset_name+'][name][" name="'+model_name+'['+fieldset_name+'][name][]" value="">');
         $(this).remove();
       }
       else if (fieldset_name != 'roles') {
         $(this).prev('input[type="text"]').remove();
-        $(this).prev('br').remove();
         $(this).before('<input type="hidden" id="'+model_name+'_'+fieldset_name+'][" name="'+model_name+'['+fieldset_name+'][]" value="">');
         $(this).remove();
       }
       
       return false;
     });
-}
+};
 
 add_person_fields = function() {
     $('.add-person-fields a').click(function() {
-      var fieldset_name = $(this).parent().parent().attr('id');
+      var fieldset_name = $(this).parents('fieldset').attr('id');
       var model_name = $(this).attr('model-name')
       var previous_select = $(this).parent().siblings('select').last();
       $(this).parent().before(
       '<select id="'+model_name+'_'+fieldset_name+'][type][" selected="'+previous_select.val()+'" name="'+model_name+'['+fieldset_name+'][type][]">'+previous_select.html()+'</select> '+  
-      '<input class="edit span6 dri-textfield" id="'+model_name+'_'+fieldset_name+'][name][" name="'+model_name+'['+fieldset_name+'][name][]" size="30" type="text" value=""> <a class="destructive" model-name="'+model_name+'">delete</a><br />');
+      '<input class="edit span6 dri-textfield" id="'+model_name+'_'+fieldset_name+'][name][" name="'+model_name+'['+fieldset_name+'][name][]" size="30" type="text" value="">  <a class="destructive" model-name="'+model_name+'">&nbsp;<i class="fa fa-times-circle"></i> Remove</a>');
       $(this).parent().siblings('select').last().val(previous_select.val())
       $(this).parent().siblings('a').last().click(function() {
         var fieldset_name = $(this).parent().attr('id');
@@ -70,7 +82,6 @@ add_person_fields = function() {
           $(this).prev('input[type="text"]').remove();
           var selected_value = $(this).prev('select').val();
           $(this).prev('select').remove();
-          $(this).prev('br').remove();
           $(this).before('<input type="hidden" id="'+model_name+'_'+fieldset_name+'][type][" name="'+model_name+'['+fieldset_name+'][type][]" value="'+selected_value+'">');
           $(this).before('<input type="hidden" id="'+model_name+'_'+fieldset_name+'][name][" name="'+model_name+'['+fieldset_name+'][name][]" value="">');
           $(this).remove();
