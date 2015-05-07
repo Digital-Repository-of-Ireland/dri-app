095cee7df4469ba980e6dbe8418216733f4f007b
Agustina Martinez <agustina.martinezgarcia@nuim.ie>
2015-05-07 19:13:39 +0100

    Action #1322 - enable MARC XSD validation; fixed md_validator so accept xmlns default ns; misc - reindex job process rels not needed but cast remains

--- lib/sufia/models/jobs/reindex_solr_job.rb
+++ lib/sufia/models/jobs/reindex_solr_job.rb
@@ -6,7 +6,9 @@ class ReindexSolrJob
 
   def run
     DRI::GenericFile.find_each { |obj| obj.update_index }
-    DRI::Batch.find_each { |obj| obj.update_index }
+    # Cast needed as the objects need to be loaded with the sub-class model (for relationships)
+    # i.e. DRI::QualifiedDublinCore, or DRI::Mods as opposed to simply DRI::Batch
+    DRI::Batch.find_each({}, {:cast => true}) { |obj| obj.update_index }
   end
 
 end
