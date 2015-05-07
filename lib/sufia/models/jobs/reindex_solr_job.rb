69ef5c73a3ac1f0ad3ab3fdc74cc2d4e6a5561d7
Agustina Martinez <agustina.martinezgarcia@nuim.ie>
2015-05-07 20:03:59 +0100

    Action #1322 - fixed md_validator; renamed marc stylesheet to record.xls as root term has changed

--- lib/sufia/models/jobs/reindex_solr_job.rb
+++ lib/sufia/models/jobs/reindex_solr_job.rb
@@ -6,12 +6,9 @@ class ReindexSolrJob
 
   def run
     DRI::GenericFile.find_each { |obj| obj.update_index }
-    # Cast needed as the objects need to be loaded with the sub-class model for relationship processing
+    # Cast needed as the objects need to be loaded with the sub-class model (for relationships)
     # i.e. DRI::QualifiedDublinCore, or DRI::Mods as opposed to simply DRI::Batch
-    DRI::Batch.find_each({}, {:cast => true}) do |obj|
-      obj.update_index
-      obj.process_relationships
-    end
+    DRI::Batch.find_each({}, {:cast => true}) { |obj| obj.update_index }
   end
 
 end
