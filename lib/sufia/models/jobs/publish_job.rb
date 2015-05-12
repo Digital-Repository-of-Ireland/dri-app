65be34fffb61d3b132445381b4808ac726eb5168
Stuart Kenny <skenny@tchpc.tcd.ie>
2015-05-12 10:01:11 +0100

    Issue #1264, add published date to DRI citation format

--- lib/sufia/models/jobs/publish_job.rb
+++ lib/sufia/models/jobs/publish_job.rb
@@ -9,8 +9,6 @@ class PublishJob < ActiveFedoraPidBasedJob
   def run
     Rails.logger.info "Publishing collection #{object.id}"
 
-    # query = Solr::Query.new("#{Solrizer.solr_name('collection_id', :facetable, type: :string)}:\"#{object.id}\" AND #{Solrizer.solr_name('status', :stored_searchable, type: :symbol)}:reviewed")
-
     # Querying by root_collection_id gets all objects, subcollections belonging to the collectionid passed as parameter
     # Publishing all object/subcollections with a reviewed status
     query = Solr::Query.new("#{Solrizer.solr_name('root_collection_id', :stored_searchable, type: :string)}:\"#{object.id}\" AND #{Solrizer.solr_name('status', :stored_searchable, type: :symbol)}:reviewed")
@@ -22,6 +20,7 @@ class PublishJob < ActiveFedoraPidBasedJob
         o = ActiveFedora::Base.find(obj["id"], {:cast => true})
         if o.status.eql?("reviewed")
           o.status = "published"
+          o.published_at = Time.now.utc.iso8601
           o.save
         end
 
@@ -31,6 +30,7 @@ class PublishJob < ActiveFedoraPidBasedJob
 
     unless object.status.eql?("published")
       object.status = "published"
+      object.published_at = Time.now.utc.iso8601
       object.save
 
       DOI.mint_doi( object )
