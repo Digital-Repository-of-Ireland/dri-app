940580de162feefd06266c06b8c99e3886fc9948
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2015-06-24 13:47:16 +0100

    Adding back in the optimised web format surrogate for inspiring ireland

--- app/models/concerns/sufia/generic_file/derivatives.rb
+++ app/models/concerns/sufia/generic_file/derivatives.rb
@@ -35,7 +35,7 @@ module Sufia
                 :large => {size: "400", datastream: 'thumbnail_large', format: 'jpg'},
                 :lightbox => {size: "600", datastream: 'lightbox_format', format: 'jpg'},
                 :full => {size: "100%", datastream: 'full_size_web_format', format: 'jpg'},
-                #:optimized => {size: "980>", datastream: 'optimized_web_format', format: 'jpg'},
+                :optimized => {size: "980>", datastream: 'optimized_web_format', format: 'jpg'},
                 :crop16_9_width_200 => {size: "200", crop: "200x113+0+0", gravity: "Center", datastream: 'crop16_9_width_200_thumbnail', format: 'jpg'}
                 #:crop16_9_width_2228 => {size: "228", crop: "228x127+0+0", gravity: "Center", datastream: 'crop16_9_width_228_thumbnail', format: 'jpg'}
               }
