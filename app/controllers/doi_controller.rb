28ad772f62dcc7c692f4a97d95a59c1e0a2b3b6d
Stuart Kenny <skenny@tchpc.tcd.ie>
2015-07-27 17:01:37 +0100

    setup test and add check for show permissions

--- app/controllers/doi_controller.rb
+++ app/controllers/doi_controller.rb
@@ -1,6 +1,8 @@
 class DoiController < ApplicationController
 
   def show
+    enforce_permissions!("show_digital_object", params[:object_id])
+
     object_id = params[:object_id]
     doi = params[:id]
 
--- spec/controllers/doi_controller_spec.rb
+++ spec/controllers/doi_controller_spec.rb
@@ -10,21 +10,23 @@ describe DoiController do
   before(:each) do
     @login_user = FactoryGirl.create(:admin)
     sign_in @login_user
+
+    @object = FactoryGirl.create(:sound)    
   end
 
   describe "GET show" do
   
     it "should assign @history" do
-      doi = DataciteDoi.create(object_id: "test")
+      doi = DataciteDoi.create(object_id: @object.id)
 
-      get :show, object_id: "test", id: doi
+      get :show, object_id: @object.id, id: doi
       expect(assigns(:history)).to eq([doi])      
     end
 
     it "should alert if doi is not the latest" do
-      doi = DataciteDoi.create(object_id: "test")
+      doi = DataciteDoi.create(object_id: @object.id)
 
-      get :show, object_id: "test", id: "test"
+      get :show, object_id: @object.id, id: "test"
       expect(flash[:notice]).to be_present
     end
 
