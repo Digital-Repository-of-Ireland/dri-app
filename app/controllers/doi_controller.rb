6eb02f285698da695950ddc3cde67f8128038bf9
Stuart Kenny <skenny@tchpc.tcd.ie>
2015-08-04 11:29:43 +0100

    issue1392 - check for nil doi config

--- app/controllers/doi_controller.rb
+++ app/controllers/doi_controller.rb
@@ -4,12 +4,19 @@ class DoiController < ApplicationController
     enforce_permissions!("show_digital_object", params[:object_id])
 
     @object_id = params[:object_id]
-    doi = "#{DoiConfig.prefix}/DRI.#{params[:id]}"
+
+    if DoiConfig.nil?
+      flash[:alert] = t('dri.flash.alert.doi_not_configured') 
+      @history = {}
+    else
+      doi = "#{DoiConfig.prefix}/DRI.#{params[:id]}"
      
-    @history = DataciteDoi.where(object_id: @object_id).ordered  
-    current = @history.first
+      @history = DataciteDoi.where(object_id: @object_id).ordered  
+      current = @history.first
     
-    flash[:notice] = t('dri.flash.notice.doi_not_latest') unless (doi == current.doi)       
+      flash[:notice] = t('dri.flash.notice.doi_not_latest') unless (doi == current.doi)       
+    end
+
   end
 
 end
