a3b438cdd212bfaae04f6ee2ed8f8ef68628de0d
Stuart Kenny <skenny@tchpc.tcd.ie>
2015-08-04 11:19:26 +0100

    issue1392 - add link to object to show view

--- app/controllers/doi_controller.rb
+++ app/controllers/doi_controller.rb
@@ -3,10 +3,10 @@ class DoiController < ApplicationController
   def show
     enforce_permissions!("show_digital_object", params[:object_id])
 
-    object_id = params[:object_id]
+    @object_id = params[:object_id]
     doi = "#{DoiConfig.prefix}/DRI.#{params[:id]}"
      
-    @history = DataciteDoi.where(object_id: object_id).ordered  
+    @history = DataciteDoi.where(object_id: @object_id).ordered  
     current = @history.first
     
     flash[:notice] = t('dri.flash.notice.doi_not_latest') unless (doi == current.doi)       
