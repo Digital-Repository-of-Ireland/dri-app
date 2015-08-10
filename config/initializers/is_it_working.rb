d4c612809952f914e2a45e048bf5cd3b9960de8a
Peter Tiernan <ptiernan@tchpc.tcd.ie>
2015-08-10 15:28:43 +0100

    uncommented unneccessary checks in is_it_working test page

--- config/initializers/is_it_working.rb
+++ config/initializers/is_it_working.rb
@@ -13,6 +13,6 @@ Rails.configuration.middleware.use(IsItWorking::Handler) do |h|
     #h.check :rubydora, :client => ActiveFedora::Base.connection_for_pid(0)
     #h.check :rsolr, :client => Dor::SearchService.solr
     #h.check :directory, :path => Rails.root + "tmp", :permission => [:read, :write]
-    h.check :ping, :host => "smtp.tchpc.tcd.ie", :port => "smtp"
-    h.check :url, :get => "http://repository.dri.ie/"
+    #h.check :ping, :host => "smtp.tchpc.tcd.ie", :port => "smtp"
+    #h.check :url, :get => "http://repository.dri.ie/"
 end
