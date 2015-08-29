29905388bf3eb2907878dea87c5a6a020e65bdbb
Stuart Kenny <skenny@tchpc.tcd.ie>
2015-08-29 19:42:51 +0100

    move helper methods to solr document models

--- app/models/concerns/dri/solr/document/relations.rb
+++ app/models/concerns/dri/solr/document/relations.rb
@@ -1,5 +1,106 @@
 module DRI::Solr::Document::Relations
 
+  # Returns an Array with all the URLS of related materials for UI Display
+  # @param[Solr::Document] document the Solr document of the object to display relations for
+  # @return Array external related materials
+  #
+  def external_relationships params
+    url_array = []
+
+    if active_fedora_model
+      case active_fedora_model
+        when 'DRI::Mods'
+          solr_fields_array = *(DRI::Vocabulary::modsRelationshipTypes.map { |s| s.prepend("ext_related_items_ids_").to_sym})
+        when 'DRI::QualifiedDublinCore'
+          solr_fields_array = *(DRI::Vocabulary::qdcRelationshipTypes.map { |s| s.prepend("ext_related_items_ids_").to_sym})
+        when 'DRI::Marc', 'DRI::EncodedArchivalDescription'
+          solr_fields_array = [:related_material, :alternative_form]
+        else
+          solr_fields_array = nil
+      end
+    end
+
+    if solr_fields_array
+      solr_fields_array.each do |elem|
+        key = ActiveFedora::SolrQueryBuilder.solr_name(elem, :stored_searchable, type: :string)
+
+        if self[key].present?
+          url_array = url_array.to_a.push(*self[key])
+        end
+      end
+    end
+    
+    Kaminari.paginate_array(url_array).page(params[:externs_page]).per(4)
+  end
+
+  # Returns a Hash with relationship groups and generated links to related objects for UI Display
+  # @param[Solr::Document] document the Solr document of the object to display relations for
+  # @return Hash related items grouped by type of relationship
+  #
+  def object_relationships params
+    relationships_hash = Hash.new
+    
+    begin
+      
+      if (active_fedora_model != "DRI::Documentation")
+        relationships_hash.merge!(get_relationships) unless (active_fedora_model == "DRI::EncodedArchivalDescription")
+        relationships_hash.merge!(get_documentation params)
+
+      elsif active_fedora_model == "DRI::Documentation"
+        documentation = documentation_for
+
+        if documentation
+          link_text = documentation[ActiveFedora::SolrQueryBuilder.solr_name('title')].first
+          relationships_hash["Is Documentation For"] = Kaminari.paginate_array([[link_text, documentation.id]]).page(params["Is Documentation For".downcase.gsub(/\s/,'_') << "_page"]).per(4)
+        end
+      end
+    rescue ActiveFedora::ObjectNotFoundError
+      Rails.logger.error("Object not found: #{document["id"]}")
+    end
+
+    return relationships_hash
+  end # get_object_relationships
+
+  private
+
+  def get_documentation params
+    docs = {}
+    doc_array = []
+    
+    documentation_ids = documentation_object_ids
+    documentation_ids.each do |id|
+      doc_obj = ActiveFedora::SolrService.query("id:#{id}", :defType => "edismax")
+
+      unless doc_obj.empty?
+        link_text = doc_obj[0][ActiveFedora::SolrQueryBuilder.solr_name('title', :stored_searchable, type: :string)].first
+        doc_array.to_a.push [link_text, doc_obj[0]["id"]]
+      end
+
+    end
+    docs["Has Documentation"] = Kaminari.paginate_array(doc_array).page(params["Has Documentation".downcase.gsub(/\s/,'_') << "_page"]).per(4) unless doc_array.empty?
+
+    docs
+  end
+
+  def get_relationships
+    rels = {}
+
+    relationships_records.each do |rel, value|
+      display_label = active_fedora_model.constantize.relationships[rel][:label]
+      item_array = []
+      value.each do |id|
+        rel_obj_doc = ActiveFedora::SolrService.query("id:#{id}", :defType => "edismax")
+        unless rel_obj_doc.empty?
+          link_text = rel_obj_doc[0][ActiveFedora::SolrQueryBuilder.solr_name('title', :stored_searchable, type: :string)].first
+          item_array.to_a.push [link_text, catalog_path(rel_obj_doc[0]["id"]).to_s]
+        end
+      end
+      rels["#{display_label}"] = Kaminari.paginate_array(item_array).page(params[display_label.downcase.gsub(/\s/,'_') << "_page"]).per(4) unless item_array.empty?
+    end # each
+
+    rels
+  end
+
   def relationships_records
     records = {}
 
