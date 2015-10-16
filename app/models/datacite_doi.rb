78588ebc934e053c361eacaf1c2ab2c77c4fba1e
Stuart Kenny <skenny@tchpc.tcd.ie>
2015-10-16 16:57:31 +0100

    #1453 try to find a creator value to use if creator not set

--- app/models/datacite_doi.rb
+++ app/models/datacite_doi.rb
@@ -54,7 +54,44 @@ class DataciteDoi < ActiveRecord::Base
   end
 
   private
-    
+
+    def creator_relators
+      doc = solr_document
+
+      people = []
+      DRI::Vocabulary.marc_relators.each do |r|
+        role = ActiveFedora::SolrQueryBuilder.solr_name("role_#{r}", :stored_searchable, type: :string)
+        people << doc[role] if doc.key?(role)
+      end
+
+      people.flatten.uniq
+    end
+
+    def creator_solr
+      doc = solr_document
+
+      key = ActiveFedora::SolrQueryBuilder.solr_name('creator', :stored_searchable, type: :string)
+
+      doc.key?(key) ? doc[key] : nil
+    end
+
+    def find_creator
+      return object.creator if object.creator.present?
+      
+      creator = creator_solr
+      return creator if creator.present?
+      
+      return object.author if object.respond_to?(:author) && object.author.present?
+      
+      creator = creator_relators
+      return creator if creator.present?
+    end
+
+    def solr_document
+      result = ActiveFedora::SolrService.query("id:#{object_id}")
+      SolrDocument.new(result.first)
+    end
+
     def set_version
       self.version = DataciteDoi.where(object_id: object_id).count
     end
@@ -62,15 +99,14 @@ class DataciteDoi < ActiveRecord::Base
     def set_metadata
       metadata = DoiMetadata.new
       metadata.title = object.title
-      metadata.creator = object.creator
-      metadata.description = object.description
-      metadata.subject = object.subject if object.subject
-      metadata.creation_date = object.creation_date if object.creation_date
-      metadata.published_date = object.published_date if object.published_date
+      metadata.creator = find_creator
+      metadata.description = object.description if object.description.present?
+      metadata.subject = object.subject if object.subject.present?
+      metadata.creation_date = object.creation_date if object.creation_date.present?
+      metadata.published_date = object.published_date if object.published_date.present?
       metadata.rights = object.rights if object.rights
       metadata.save
 
       self.doi_metadata = metadata
     end
-
 end
