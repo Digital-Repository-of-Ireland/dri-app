9084471afb139ea2a562a6802a6dba63a17295b5
Juliusz Filipowski <filipowj@tchpc.tcd.ie>
2015-11-17 12:06:09 +0000

    Fix bug #1320, add initialize method to change minter behaviour

--- config/initializers/assign_pid.rb
+++ config/initializers/assign_pid.rb
@@ -1,7 +1,27 @@
-module ActiveFedora
-  class UnsavedDigitalObject
-    def assign_pid
-      @pid ||= ActiveFedora::Noid::Service.new.mint
+require "sufia/noid.rb"
+
+Sufia::Noid.module_eval do
+
+  def assign_id
+    if Sufia.config.enable_noids
+      id = nil
+      id = new_id until id
+
+      id
     end
   end
+
+  def new_id
+    id = service.mint
+    ActiveFedora::Base.find(id)
+
+    id
+  rescue Ldp::Gone
+    Rails.logger.error "Tombstone ID #{id}"
+    nil
+  rescue ActiveFedora::ObjectNotFoundError
+    id
+  end
+
 end
+
