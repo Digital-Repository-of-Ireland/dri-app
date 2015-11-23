fd8b6ef9d664a14c4cdd2706bcbfc0f9b1dac322
Juliusz Filipowski <filipowj@tchpc.tcd.ie>
2015-11-23 11:05:47 +0000

    add status.rb to background_tasks, follow after develop branch

---
+++ lib/background_tasks/status.rb
@@ -0,0 +1,58 @@
+module BackgroundTasks
+  module Status
+
+    def status
+      @status ||= IngestStatus.find_or_create_by(asset_id: generic_file_id) do |ingest_status|
+        ingest_status.batch_id = generic_file.batch.id
+        ingest_status.status = 'processing'
+      end
+
+      if @status.status == 'success'
+        @status.status = 'processing'
+        @status.job_status.each { |j| j.delete }
+        @status.save
+      end
+
+      @status
+    end
+
+    def with_status_update(job)
+      create_job_status(job)
+      status.job_status << @job_status
+      status.save
+
+      begin
+        yield
+
+        success
+      rescue => e
+        Rails.logger.error "Error in #{job} background task for #{generic_file_id}: #{e.message}"
+        failed(e.message)
+      end
+    end
+
+    def create_job_status(job)
+      @job_status = JobStatus.create(job: job, status: 'processing')
+    end
+
+    def failed(message)
+      @job_status.status = 'failed'
+      @job_status.message = message
+      @job_status.save
+
+      status.completed?(@job_status)
+    end
+
+    def status_for_type(type)
+      status.asset_type = type
+      status.save
+    end
+
+    def success
+      @job_status.status = 'success'
+      @job_status.save
+
+      status.completed?(@job_status)
+    end
+  end
+end
