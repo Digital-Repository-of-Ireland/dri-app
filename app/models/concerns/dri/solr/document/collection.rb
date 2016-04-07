961cb92653a9fc79c74e1bb611328d662e430872
Stuart Kenny <skenny@tchpc.tcd.ie>
2016-04-07 10:18:17 +0100

    #437 display duplicates count if any under review menu. Also display on review confirm popup

--- app/models/concerns/dri/solr/document/collection.rb
+++ app/models/concerns/dri/solr/document/collection.rb
@@ -12,12 +12,41 @@ module DRI::Solr::Document::Collection
     status_count('reviewed')
   end
 
+  def duplicate_objects
+    find_duplicates
+  end
+
   private
 
   def status_count(status)
-    ActiveFedora::SolrService.count("#{ActiveFedora::SolrQueryBuilder.solr_name('ancestor_id', :facetable, type: :string)}:#{self.id} 
-      AND #{ActiveFedora::SolrQueryBuilder.solr_name('status', :stored_searchable, type: :symbol)}:#{status}")
+    ActiveFedora::SolrService.count("#{ActiveFedora.index_field_mapper.solr_name('ancestor_id', :facetable, type: :string)}:#{self.id} 
+      AND #{ActiveFedora.index_field_mapper.solr_name('status', :stored_searchable, type: :symbol)}:#{status}")
   end
 
+  def find_duplicates
+    metadata_field = ActiveFedora.index_field_mapper.solr_name('metadata_md5', :stored_searchable, type: :string)
+
+    response = ActiveFedora::SolrService.get('*:*', 
+      fq: ["+#{ActiveFedora.index_field_mapper.solr_name('ancestor_id', :facetable, type: :string)}:#{self.id}", 
+        "+has_model_ssim:\"DRI::Batch\"", "+is_collection_sim:false"], 
+        "facet.pivot" => "#{metadata_field},id", 
+        facet: true, 
+        "facet.mincount" => 2, 
+        "facet.field" => "#{metadata_field}")
+
+    duplicates = response['facet_counts']['facet_fields']["#{metadata_field}"]
+    total = 0
+    md5 = []
+
+    duplicates.each_slice(2) do |duplicate|
+      total += duplicate[1].to_i
+      md5 << duplicate[0]
+    end
+
+    query = ''
+    query = md5.join(' OR ') if md5.count > 0
+
+    return query, total
+  end
 
 end
