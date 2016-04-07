10cac7af8e436dc53eceb850da73319cdcb60eea
Stuart Kenny <skenny@tchpc.tcd.ie>
2016-04-07 16:57:15 +0100

    #1515 #1514 ajax pagination with kaminari and add subcollection count

--- app/helpers/document_helper.rb
+++ app/helpers/document_helper.rb
@@ -1,5 +1,11 @@
 module DocumentHelper
 
+  Child = Struct.new(:link_text, :path, :type) do
+    def to_partial_path
+      "child"
+    end
+  end
+
   def get_collection_media_type_params(document, collectionId, mediaType)
     if document[Solrizer.solr_name('collection_id', :stored_searchable, type: :string)] == nil
       searchFacets = { Solrizer.solr_name('file_type_display', :facetable, type: :string).to_sym => [mediaType], Solrizer.solr_name('root_collection_id', :facetable, type: :string).to_sym => [collectionId] }
@@ -23,28 +29,26 @@ module DocumentHelper
 
   # For a given collection (sub-collection) object
   # returns a list of the immediate child sub-collections
-  def get_collection_children(document, limit)
+  def collection_children(document, limit)
     children_array = []
-    # Find immediate children of this collection
-    solr_query = "#{Solrizer.solr_name('collection_id', :stored_searchable, type: :string)}:\"#{document['id']}\""
-    f_query = "#{Solrizer.solr_name('is_collection', :stored_searchable, type: :string)}:true"
-
-    # Filter to only get those that are collections:
-    # fq=is_collection_tesim:true
-    q_result = Solr::Query.new(solr_query, limit, fq: f_query)
-    q_result.each_solr_document do |doc|
+    children = document.children(limit)
+    
+    children.each do |doc|
       next unless doc.published? || ((current_user && current_user.is_admin?) || can?(:edit, doc))
 
       link_text = doc[Solrizer.solr_name('title', :stored_searchable, type: :string)].first
       # FIXME: For now, the EAD type is indexed last in the type solr index, review in the future
       type = doc[Solrizer.solr_name('type', :stored_searchable, type: :string)].last
 
-      children_array = children_array.to_a.push([link_text, catalog_path(doc['id']).to_s, type.to_s])
+      child = Child.new
+      child.link_text = link_text
+      child.path = catalog_path(doc['id'])
+      child.type = type
+      
+      children_array = children_array.to_a.push( child )
     end
 
-    paged_children = Kaminari.paginate_array(children_array).page(params[:subs_page]).per(4)
-
-    paged_children
+    Kaminari.paginate_array(children_array).page(params[:subs_page]).per(4)
   end
 
   def get_object_relationships(document)
--- app/models/concerns/dri/solr/document/collection.rb
+++ app/models/concerns/dri/solr/document/collection.rb
@@ -1,5 +1,21 @@
 module DRI::Solr::Document::Collection
 
+  def children(limit)
+    children_array = []
+    # Find immediate children of this collection
+    solr_query = "#{Solrizer.solr_name('collection_id', :stored_searchable, type: :string)}:\"#{self.id}\""
+    f_query = "#{Solrizer.solr_name('is_collection', :stored_searchable, type: :string)}:true"
+
+    # Filter to only get those that are collections:
+    # fq=is_collection_tesim:true
+    q_result = Solr::Query.new(solr_query, limit, fq: f_query)
+    q_result.each_solr_document do |doc|
+      children_array << doc
+    end
+
+    children_array
+  end
+
   def draft_objects
     status_count('draft')
   end
