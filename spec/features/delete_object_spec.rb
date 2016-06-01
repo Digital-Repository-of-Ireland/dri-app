a2cb0efeef154624d54d409b572c2d05fbd02e2a
Stuart Kenny <skenny@tchpc.tcd.ie>
2016-06-01 14:32:19 +0100

    #1545 fix failing test

--- spec/features/delete_object_spec.rb
+++ spec/features/delete_object_spec.rb
@@ -17,15 +17,25 @@ feature 'Deleting a single object' do
 
   scenario 'unable to delete a published object' do
     collection = FactoryGirl.create(:collection)
-   
+    collection.depositor = User.find_by_email(@login_user.email).to_s
+    collection.manager_users_string=User.find_by_email(@login_user.email).to_s
+    collection.discover_groups_string="public"
+    collection.read_groups_string="registered"
+    collection.creator = [@login_user.email]
+    collection.save
+
     object = FactoryGirl.create(:sound) 
     object[:status] = "published"
+    object.depositor=User.find_by_email(@login_user.email).to_s
+    object.manager_users_string=User.find_by_email(@login_user.email).to_s
+    object.creator = [@login_user.email]  
+  
     object.save
 
     collection.governed_items << object
   
     visit(object_path(object.id))
-
+    
     expect(page).not_to have_link(I18n.t('dri.views.objects.buttons.delete_object'))
 
     collection.delete
@@ -33,15 +43,25 @@ feature 'Deleting a single object' do
 
   scenario 'deleting a draft object' do
     collection = FactoryGirl.create(:collection)
-   
-    object = FactoryGirl.create(:sound) 
+    collection.depositor = User.find_by_email(@login_user.email).to_s
+    collection.manager_users_string=User.find_by_email(@login_user.email).to_s
+    collection.discover_groups_string="public"
+    collection.read_groups_string="registered"
+    collection.creator = [@login_user.email]
+    collection.save
+
+    object = FactoryGirl.create(:sound)
     object[:status] = "draft"
+    object.depositor=User.find_by_email(@login_user.email).to_s
+    object.manager_users_string=User.find_by_email(@login_user.email).to_s
+    object.creator = [@login_user.email]
+
     object.save
 
     collection.governed_items << object
   
     visit(object_path(object.id))
-    click_button "delete_object_#{object.id}"
+    click_button "submit_delete"
 
     expect(current_path).to eq(root_path)
     expect(page.find('.dri_alert_text')).to have_content I18n.t('dri.flash.notice.object_deleted')
