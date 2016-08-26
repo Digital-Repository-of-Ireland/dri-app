ba84d00c0fea4d7ea379b6f6f919d6d94ddc7bd0
Stuart Kenny <skenny@tchpc.tcd.ie>
2016-08-26 14:58:09 +0100

    #1561 working manifest

--- app/controllers/concerns/dri/iiif_viewable.rb
+++ app/controllers/concerns/dri/iiif_viewable.rb
@@ -6,6 +6,7 @@ module DRI::IIIFViewable
   WIDTH_SOLR_FIELD = 'width_isi'
 
   def iiif_manifest
+    object_url = url_for controller: 'objects', action: 'show', id: @object.id
     seed_id = url_for controller: 'objects', action: 'manifest', id: @object.id, format: 'json'
 
     seed = {
@@ -19,7 +20,7 @@ module DRI::IIIFViewable
     files = attached_images
     files.each do |f| 
       canvas = IIIF::Presentation::Canvas.new()
-      canvas['@id'] = "#{seed_id}/canvas/#{f.id}"
+      canvas['@id'] = "#{object_url}/canvas/#{f.id}"
       # ...but there are also accessors and mutators for the properties mentioned in 
       # the spec
       canvas.width = f[HEIGHT_SOLR_FIELD]
@@ -27,17 +28,23 @@ module DRI::IIIFViewable
       canvas.label = f[ActiveFedora.index_field_mapper.solr_name('label')]
 
       image_url = Riiif::Engine.routes.url_for controller: 'riiif/images', action: 'show', 
-          id: f.id, region: 'full', size: 'full', rotation: 0, quality: 'default', format: 'jpg', only_path: true
+          id: f.id, region: 'full', size: 'full', rotation: 0, quality: 'default', format: 'jpg', only_path: true   
 
-      image = IIIF::Presentation::ImageResource.new({'@id' => "#{root_url}#{image_url}"})
-      image.width = f[WIDTH_SOLR_FIELD]
-      image.height = f[HEIGHT_SOLR_FIELD]
+      image = IIIF::Presentation::ImageResource.create_image_api_image_resource({resource_id: "#{root_url}#{image_url}",
+        service_id: "#{root_url}/images/#{f.id}",
+        width: f[WIDTH_SOLR_FIELD], height: f[HEIGHT_SOLR_FIELD],
+        profile: 'http://iiif.io/api/image/2/profiles/level2.json'})
+      image['@type'] = 'dctypes:Image'
 
-      image.service = IIIF::Service.new({'@id' => "#{root_url}/images/#{f.id}"})
+      annotation = IIIF::Presentation::Annotation.new
+      annotation['on'] = canvas['@id']
+      annotation.resource = image
 
-      canvas.images << image
+      canvas.images << annotation
+      sequence = IIIF::Presentation::Sequence.new({'@id' => "#{object_url}/sequence/normal", 'viewing_hint' => 'individuals'})
+      sequence.canvases << canvas   
 
-      manifest.sequences << canvas
+      manifest.sequences << sequence
     end
 
     manifest
