d6617c77a54607a03c8849faee9fafd95e6cbcd2
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2016-09-19 14:55:18 +0100

    Added preservation calls to new edit access controls functions

--- app/controllers/access_controls_controller.rb
+++ app/controllers/access_controls_controller.rb
@@ -16,7 +16,10 @@ class AccessControlsController < ApplicationController
     params[:batch][:read_users_string] = params[:batch][:read_users_string].to_s.downcase
     params[:batch][:edit_users_string] = params[:batch][:edit_users_string].to_s.downcase
     params[:batch][:manager_users_string] = params[:batch][:manager_users_string].to_s.downcase if params[:batch][:manager_users_string].present?
-    
+    params[:batch][:object_version] = @object.object_version.to_i+1
+
+    permissionchange = permissions_changed?
+
     if @object.collection?
       if valid_permissions?
         updated = @object.update_attributes(update_params)
@@ -25,14 +28,22 @@ class AccessControlsController < ApplicationController
       updated = @object.update_attributes(update_params)
     end
 
-    #purge params from update action
-    purge_params
-
     if updated
       flash[:notice] = t('dri.flash.notice.access_controls_updated')
+
+      # Do the preservation actions
+      preservation = Preservation::Preservator.new(@object)
+      if permissionchange
+        preservation.preserve(false, true, ['properties'])
+      else 
+        preservation.preserve(false, false, ['properties'])
+      end
     else
       flash[:alert] = t('dri.flash.error.not_updated', item: params[:id])
     end
+
+    #purge params from update action
+    purge_params
 
     respond_to do |format|
       format.html  { redirect_to controller: 'catalog', action: 'show', id: @object.id }
@@ -51,7 +62,7 @@ class AccessControlsController < ApplicationController
     end
 
     def update_params
-      params.require(:batch).permit(:read_groups_string, :read_users_string, :master_file_access, :edit_groups_string, :edit_users_string, :manager_users_string)
+      params.require(:batch).permit(:read_groups_string, :read_users_string, :master_file_access, :edit_groups_string, :edit_users_string, :manager_users_string, :object_version)
     end
 
     def valid_permissions?
@@ -63,4 +74,14 @@ class AccessControlsController < ApplicationController
         true
       end
     end
+
+    def permissions_changed?
+      if (@object.read_groups_string.eql?(params[:batch][:read_groups_string]) &&
+          @object.edit_users_string.eql?(params[:batch][:edit_users_string]) &&
+          @object.manager_users_string.eql?(params[:batch][:manager_users_string]) )
+         return false
+      end
+      return true
+    end
+
 end
