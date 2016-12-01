95743b34670d8e78d95f011b28191a3fe8a927b3
Stuart Kenny <skenny@tchpc.tcd.ie>
2016-12-01 10:58:44 +0000

    #1595 upgrade to rspec 3

--- spec/controllers/collections_spec.rb
+++ spec/controllers/collections_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe CollectionsController do
   include Devise::Test::ControllerHelpers
--- spec/controllers/doi_controller_spec.rb
+++ spec/controllers/doi_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe DoiController do
   include Devise::Test::ControllerHelpers
