cfa6bf5ecb2b3ee1ce7dd5d2ccd6adaec7aea6fe
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2017-02-06 13:39:39 +0000

    More changes for issue #1367

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -156,4 +156,24 @@ module UserHelper
 
     facets
   end
+
+  def get_inherited_read_groups(obj)
+    return if obj == nil
+    if obj.read_groups.empty?
+      get_inherited_read_groups(obj.governing_collection)
+    elsif obj.read_groups.first == 'registered'
+      return "logged-in"
+    elsif obj.read_groups.first == 'public'
+      return "public"
+    else 
+      return "restricted"
+    end
+  end
+
+  def get_inherited_masterfile_access(obj)
+    return if obj == nil
+    return obj.master_file_access unless obj.master_file_access == "inherit" || obj.master_file_access == nil
+    get_inherited_masterfile_access(obj.governing_collection)
+  end
+
 end
