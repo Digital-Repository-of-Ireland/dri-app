01d805747768c4b589390e2848d2c6ff460d2bd3
Stuart Kenny <skenny@tchpc.tcd.ie>
2017-03-21 14:23:22 +0000

    #1658 #1657 filesystem as well as S3 and exclude preservation only

--- app/jobs/create_archive_job.rb
+++ app/jobs/create_archive_job.rb
@@ -4,61 +4,72 @@ class CreateArchiveJob
   @queue = :create_archive
 
   def self.perform(object_id, email) 
-      object = ActiveFedora::Base.find(object_id, cast: true)
+    object = ActiveFedora::Base.find(object_id, cast: true)
+    
+    # Get metadata
+    # Get assets if masterfile
+    # Get surrogates if read
+    # Actually, if not read cannot ever download the archive
 
-      # Get metadata
-      # Get assets if masterfile
-      # Get surrogates if read
-      # Actually, if not read cannot ever download the archive
+    # 'optimised' directory containing all surrogates
+    # 'originals' directory containing all master assets if master asset is accessible
 
-      # 'optimised' directory containing all surrogates
-      # 'originals' directory containing all master assets if master asset is accessible
+    Rails.logger.info "Creating archive package for object #{object.id}"
 
-      Rails.logger.info "Creating archive package for object #{object.id}"
+    # pick the filname and location
+    tmp = Tempfile.new("#{object.id}_")
+    zipfile = Zip::File.open(tmp.path, Zip::File::CREATE)
 
-      # pick the filname and location
-      tmp = Tempfile.new("#{object.id}_")
-      zipfile = Zip::File.open(tmp.path, Zip::File::CREATE)
+    # descMetadata
+    metadata = Tempfile.new('descMetadata.xml')
+    metadata.write(object.attached_files['descMetadata'].content)
+    metadata.close
+    zipfile.add("#{object.id}/descMetadata.xml", metadata.path)
 
-      # descMetadata
-      metadata = Tempfile.new('descMetadata.xml')
-      metadata.write(object.attached_files['descMetadata'].content)
-      metadata.close
-      zipfile.add("#{object.id}/descMetadata.xml", metadata.path)
+    # Licence.txt
+    licence = Tempfile.new('Licence.txt')
+    licence.puts("Rights Statement: #{object.rights.join()}")
+    licence.puts("Licence: #{get_inherited_licence(object)}")
+    licence.close
+    zipfile.add("#{object.id}/Licence.txt", licence.path)
 
-      # Licence.txt
-      licence = Tempfile.new('Licence.txt')
-      licence.puts("Rights Statement: #{object.rights.join()}")
-      licence.puts("Licence: #{get_inherited_licence(object)}")
-      licence.close
-      zipfile.add("#{object.id}/Licence.txt", licence.path)
+    storage = StorageService.new
 
-      storage = StorageService.new
-
-      object.generic_files.each do |gf|
-        if get_inherited_masterfile_access(object) == "public"
-          lf = LocalFile.where('fedora_id LIKE :f AND ds_id LIKE :d', f: gf.id, d: 'content').order('version DESC').to_a.first
-          zipfile.add("#{object.id}/originals/#{gf.id}_#{gf.label}", lf.path)
-        end
-
-        # Get surrogates
-        surrogate = Tempfile.new('surrogate')
-        surrogate.binmode
-        surrogate.write HTTParty.get(storage.surrogate_url(object.id,"#{gf.id}_full_size_web_format")).parsed_response
-        surrogate.close
-        zipfile.add("#{object.id}/optimised/#{gf.id}_optimised_#{gf.label}", surrogate)
+    object.generic_files.each do |gf|
+      # exclude presevation files
+      next if gf.preservation_only == 'true'
+      
+      if get_inherited_masterfile_access(object) == "public"
+        lf = LocalFile.where('fedora_id LIKE :f AND ds_id LIKE :d', f: gf.id, d: 'content').order('version DESC').to_a.first
+        zipfile.add("#{object.id}/originals/#{gf.id}_#{gf.label}", lf.path)
       end
 
-      zipfile.close
-      metadata.unlink
-      licence.unlink
+      # Get surrogates
+      surrogate = Tempfile.new('surrogate')
+      storage_url = storage.surrogate_url(object.id,"#{gf.id}_full_size_web_format")
 
-      # raise "Unable to create storage bucket" unless created
-      JobMailer.archive_ready_mail(File.basename(tmp.path), email, object).deliver_now
+      # handle case of using file storage as well as S3
+      if storage_url =~ /\A#{URI.regexp(['http', 'https'])}\z/
+        surrogate.binmode
+        surrogate.write HTTParty.get(storage_url).parsed_response
+        surrogate.close
+      else
+        FileUtils.cp(storage_url, surrogate.path)
+      end
 
-      # Move the file to the downloads dir
-      FileUtils.mv(tmp.path, Settings.dri.downloads)
-      tmp.unlink
+      zipfile.add("#{object.id}/optimised/#{gf.id}_optimised_#{gf.label}", surrogate)
+    end
+
+    zipfile.close
+    metadata.unlink
+    licence.unlink
+
+    # raise "Unable to create storage bucket" unless created
+    JobMailer.archive_ready_mail(File.basename(tmp.path), email, object).deliver_now
+
+    # Move the file to the downloads dir
+    FileUtils.mv(tmp.path, Settings.dri.downloads)
+    tmp.unlink
   end
 
   def self.get_inherited_licence(obj)
