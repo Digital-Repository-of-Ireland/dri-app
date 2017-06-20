122732c8a3400bcc78ead9e0a651d39aaa0e194c
Stuart Kenny <skenny@tchpc.tcd.ie>
2017-06-20 19:09:41 +0100

    #1705 pass requesting controller name as param

--- config/initializers/blacklight_maps_overrides.rb
+++ config/initializers/blacklight_maps_overrides.rb
@@ -4,7 +4,7 @@ Blacklight::BlacklightMapsHelperBehavior.module_eval do
   def link_to_bbox_search bbox_coordinates
     coords_for_search = bbox_coordinates.map { |v| v.to_s }
     link_to(t('blacklight.maps.interactions.bbox_search'),
-            url_for(controller: controller_name, action: 'index', only_path: true, spatial_search_type: "bbox",
+            url_for(controller: request_controller, action: 'index', only_path: true, spatial_search_type: "bbox",
                                coordinates: "[#{coords_for_search[1]},#{coords_for_search[0]} TO #{coords_for_search[3]},#{coords_for_search[2]}]",
                                view: default_document_index_view_type))
   end
@@ -17,8 +17,9 @@ Blacklight::BlacklightMapsHelperBehavior.module_eval do
       new_params = add_facet_params(field, field_value)
     end
     new_params[:view] = default_document_index_view_type
+    
     link_to(displayvalue.presence || field_value,
-            url_for({controller: controller_name, action: 'index', only_path: true}.merge(
+            url_for({controller: request_controller, action: 'index', only_path: true}.merge(
               new_params.except(:id, :spatial_search_type, :coordinates))
             )
           )
@@ -30,7 +31,11 @@ Blacklight::BlacklightMapsHelperBehavior.module_eval do
     new_params[:spatial_search_type] = "point"
     new_params[:coordinates] = "#{point_coordinates[1]},#{point_coordinates[0]}"
     new_params[:view] = default_document_index_view_type
-    link_to(t('blacklight.maps.interactions.point_search'), url_for({controller: controller_name, action: 'index', only_path: true}.merge(new_params)))
+    link_to(t('blacklight.maps.interactions.point_search'), url_for({controller: request_controller, action: 'index', only_path: true}.merge(new_params)))
+  end
+
+  def request_controller
+    params[:request_controller].presence || controller_name
   end
 
 end
