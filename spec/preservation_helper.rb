6010ef4b0e78df6653475a6630aaf1a9a70f6089
Stuart Kenny <skenny@tchpc.tcd.ie>
2017-06-21 12:54:58 +0100

    move flapping test to rspec

--- spec/preservation_helper.rb
+++ spec/preservation_helper.rb
@@ -10,4 +10,12 @@ module PreservationHelper
 
     File.join(Settings.dri.files, dir, id)
   end
+
+  def aip_valid?(id, version)
+    storage_object = Moab::StorageObject.new(id, aip_dir(id))
+    storage_object_version = Moab::StorageObjectVersion.new(storage_object, version_id=version)
+    result = storage_object_version.verify_version_storage
+    result.verified
+  end
+
 end
