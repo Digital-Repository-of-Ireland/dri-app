cafbc00afad36da537735217ddbc5bf96a02392e
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2017-07-06 15:46:55 +0100

    Starting to implement reports as reviewed at staff meeting

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,11 +2,16 @@ class AnalyticsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    respond_to do |format|
-      format.html
-      format.json do
-        render json: CollectionViewsDatatable.new(view_context)
+    if signed_in? && (current_user.is_admin? || current_user.is_om? || current_user.is_cm?)
+
+      respond_to do |format|
+        format.html
+        format.json do
+          render json: CollectionViewsDatatable.new(view_context)
+        end
       end
+    else
+      flash[:error] = t('dri.flash.error.manager_user_permission')
     end
   end
 
