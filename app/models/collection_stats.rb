e907d19882a053c1859b0f0a8218d0945f1ccc45
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2017-07-11 16:45:00 +0100

    Legato model for Google Analytics

---
+++ app/models/collection_stats.rb
@@ -0,0 +1,8 @@
+class CollectionStats
+    extend Legato::Model
+
+    filter :collections, &lambda {|*collections| collections.map {|collectionid| matches(:dimension1, collectionid)}}
+
+    metrics :users
+    dimensions :dimension1
+end
