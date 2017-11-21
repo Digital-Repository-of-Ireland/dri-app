958a7436c4b83c7717ba3e8facc663e15889e942
Stuart Kenny <skenny@tchpc.tcd.ie>
2017-11-21 16:32:11 +0000

    save state of datatables

--- app/assets/javascripts/datatables.js
+++ app/assets/javascripts/datatables.js
@@ -20,6 +20,7 @@ $(document).ready(function() {
     $('#datatable_activity').DataTable( {
         "processing": true,
         "serverSide": true,
+        "stateSave": true,
         "order": [[ 0, "desc" ]],
         "ajax": $('#datatable_activity').data('source'),
         columnDefs: [
@@ -34,6 +35,7 @@ $(document).ready(function() {
         "processing": true,
         "serverSide": true,
         "searching": false,
+        "stateSave": true,
         "bInfo" : false,
         "order": [[ 0, "desc" ]],
         "ajax": $('#datatable_fixity').data('source'),
@@ -64,6 +66,7 @@ $(document).ready(function() {
     $('#datatable_user_activity').DataTable( {
         "processing": true,
         "serverSide": true,
+        "stateSave": true,
         "order": [[ 0, "desc" ]],
         "ajax": $('#datatable_user_activity').data('source'),
         columnDefs: [
