01fb736a2da0ec36a1618cc04eefd1c26a32d28e
Stuart Kenny <skenny@tchpc.tcd.ie>
2018-03-21 11:57:01 +0000

    #1759 add rspec tests

---
+++ spec/jobs/status_job_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+require 'ostruct'
+require 'resque-status'
+
+describe 'StatusJob' do
+	
+   describe 'run' do
+
+     it 'should return call update with completed counts' do
+       allow_any_instance_of(StatusJob).to receive(:retrieve_status).and_return(
+                     [['test1', OpenStruct.new({ status: 'completed' })],['test2', OpenStruct.new({ status: 'completed' })]])
+
+       job = StatusJob.new('test')
+
+       expect(job).to receive(:update_status).once.ordered.with('test', 2, 1)
+       expect(job).to receive(:update_status).once.ordered.with('test', 2, 2)
+       job.wait_for_completion('test', ['test1', 'test2'])
+     end
+
+   end
+
+end
