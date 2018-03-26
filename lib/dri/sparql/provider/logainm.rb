12a3034aea22fff1544b4afe0797b40a2bffb886
Stuart Kenny <skenny@tchpc.tcd.ie>
2018-03-26 15:57:09 +0100

    #1760 check for logainm web urls and transform if needed

--- lib/dri/sparql/provider/logainm.rb
+++ lib/dri/sparql/provider/logainm.rb
@@ -1,41 +1,52 @@
 require 'dri/sparql'
 
-module DRI::Sparql
-  module Provider
-    class Logainm
+module DRI::Sparql::Provider
+  class Logainm
 
-      def endpoint=(endpoint)
-        @endpoint = endpoint
+    def endpoint=(endpoint)
+      @endpoint = endpoint
+    end
+
+    def retrieve_data(uri)
+      return unless DRI::LinkedData.where(source: uri).empty?
+
+      select = "select ?nameEN, ?nameGA, ?lat, ?long
+                where { <#{transform_uri(uri)}> <http://xmlns.com/foaf/0.1/name> ?nameGA, ?nameEN;
+                <http://geovocab.org/geometry#geometry> ?g . ?g geo:lat ?lat; geo:long ?long .
+                filter(lang(?nameEN)=\"en\" && lang(?nameGA)=\"ga\") . }"
+
+      client = DRI::Sparql::Client.new @endpoint
+      results = client.query select
+      
+      points = []
+      unless results.nil?
+        results.each_solution do |s|
+          name = "#{s[:nameGA].value}/#{s[:nameEN]}"
+          north = s[:lat].value
+          east = s[:long].value
+          points << DRI::Metadata::Transformations::SpatialTransformations.coords_to_geojson_string(name, "#{east} #{north}")
+        end
       end
 
-      def retrieve_data(uri)
-        return unless DRI::LinkedData.where(source: uri).empty?
+      return unless points.present?
 
-        select = "select ?nameEN, ?nameGA, ?lat, ?long
-                  where { <#{uri}> <http://xmlns.com/foaf/0.1/name> ?nameGA, ?nameEN;
-                  <http://geovocab.org/geometry#geometry> ?g . ?g geo:lat ?lat; geo:long ?long .
-                  filter(lang(?nameEN)=\"en\" && lang(?nameGA)=\"ga\") . }"
+      linked = DRI::LinkedData.new
+      linked.source = [uri]
+      linked.resource_type = ['Dataset']
+      linked.spatial = points
+      linked.save
+    end
 
-        client = DRI::Sparql::Client.new @endpoint
-        results = client.query select
-        
-        points = []
-        unless results.nil?
-          results.each_solution do |s|
-            name = "#{s[:nameGA].value}/#{s[:nameEN]}"
-            north = s[:lat].value
-            east = s[:long].value
-            points << DRI::Metadata::Transformations::SpatialTransformations.coords_to_geojson_string(name, "#{east} #{north}")
-          end
-        end
+    def transform_uri(uri)
+      host = URI(uri).host
 
-        return unless points.present?
+      if host == 'www.logainm.ie'
+        name = uri[%r{[^/]+\z}]
+        place_id = File.basename(name,File.extname(name))
 
-        linked = DRI::LinkedData.new
-        linked.source = [uri]
-        linked.resource_type = ['Dataset']
-        linked.spatial = points
-        linked.save
+        "http://data.logainm.ie/place/#{place_id}"
+      else
+        uri
       end
     end
   end
