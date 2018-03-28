d62b774d3d28bd2f044500db41a90575213bd9d4
Stuart Kenny <skenny@tchpc.tcd.ie>
2018-03-28 16:44:37 +0100

    #1762 query for latest checks

--- app/models/fixity_check.rb
+++ app/models/fixity_check.rb
@@ -1,4 +1,4 @@
 class FixityCheck < ActiveRecord::Base
-  scope :latest, -> { group(:object_id).having('created_at = MAX(created_at)') }
+  scope :latest, -> { select('id, collection_id, object_id, MAX(created_at) as created_at, MAX(created_at) as max_created_at').group(:object_id).order('created_at') }
   scope :failed, -> { latest.where(verified: false) }
 end
