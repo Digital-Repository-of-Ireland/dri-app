3920f1b712f733b15aa7cb9a48da7311e6ce67a8
Stuart Kenny <skenny@tchpc.tcd.ie>
2018-05-18 15:21:57 +0100

    remove some methods from helpers

--- app/controllers/workspace_controller.rb
+++ app/controllers/workspace_controller.rb
@@ -4,6 +4,7 @@ class WorkspaceController < ApplicationController
   before_action :authenticate_user!
 
   def index
+     @tasks_count = UserBackgroundTask.where(user_id: current_user.id).count
   end
 
 end
