c0db43aff7e2f763b6f7e06e92894ee9f3f8b50e
skenny <skenny@tchpc.tcd.ie>
2018-08-27 16:52:00 +0100

    #1814 extra tests for batch ingest job

--- spec/jobs/process_batch_ingest_spec.rb
+++ spec/jobs/process_batch_ingest_spec.rb
@@ -27,8 +27,9 @@ describe 'ProcessBatchIngest' do
       tmp_file = Tempfile.new(['metadata', '.xml'])
       FileUtils.cp(File.join(fixture_path, 'valid_metadata.xml'), tmp_file.path)
       metadata = { master_file_id: master_file.id, path: tmp_file.path }
-      object = ProcessBatchIngest.ingest_metadata(@collection, @login_user, metadata)
+      rc, object = ProcessBatchIngest.ingest_metadata(@collection, @login_user, metadata)
 
+      expect(rc).to eq 0
       expect(object.valid?).to be true
       expect(object.persisted?).to be true
     end
@@ -52,4 +53,61 @@ describe 'ProcessBatchIngest' do
 
   end
 
+  context "ingest errors" do
+
+    let(:master_file) { DriBatchIngest::MasterFile.create }
+
+    it "should rescue errors saving metadata" do
+      allow_any_instance_of(DRI::Noid::Service).to receive(:mint).and_raise(Ldp::HttpError)
+
+      tmp_file = Tempfile.new(['metadata', '.xml'])
+      FileUtils.cp(File.join(fixture_path, 'valid_metadata.xml'), tmp_file.path)
+      metadata = { master_file_id: master_file.id, path: tmp_file.path }
+      rc, object = ProcessBatchIngest.ingest_metadata(@collection, @login_user, metadata)
+
+      master_file.reload
+
+      expect(rc).to eq -1
+      expect(object.persisted?).to be false
+      expect(master_file.status_code).to eq 'FAILED'
+    end
+
+     it "should rescue errors saving invalid metadata" do
+      tmp_file = Tempfile.new(['metadata', '.xml'])
+      FileUtils.cp(File.join(fixture_path, 'metadata_no_rights.xml'), tmp_file.path)
+      metadata = { master_file_id: master_file.id, path: tmp_file.path }
+      rc, object = ProcessBatchIngest.ingest_metadata(@collection, @login_user, metadata)
+
+      master_file.reload
+
+      expect(rc).to eq -1
+      expect(object.persisted?).to be false
+      expect(master_file.status_code).to eq 'FAILED'
+    end
+
+    it "should rescue errors if files not found" do
+      Settings.downloads.directory = Dir.mktmpdir
+
+      allow_any_instance_of(BrowseEverything::Retriever).to receive(:retrieve).and_raise(Errno::ENOENT)
+      tmp_file = Tempfile.new(['metadata', '.xml'])
+
+      ingest_message = {}
+      ingest_message['collection'] = @collection_id
+      ingest_message['media_object'] = 1
+      ingest_message['metadata'] = {
+        'download_spec' => {
+        'url' => "file://#{tmp_file.path}",
+        'file_name' => File.basename(tmp_file.path) }
+      }
+      ingest_message['files'] = []
+
+      rc, object = ProcessBatchIngest.perform(@login_user.id, @collection.id, ingest_message.to_json)
+
+      master_file.reload
+
+      expect(rc).to eq -1
+      expect(object.persisted?).to be false
+      expect(master_file.status_code).to eq 'FAILED'
+    end
+  end
 end
