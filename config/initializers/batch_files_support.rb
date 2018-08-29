3d6819aee20a7af31b40c78096a91f31ddfa3d59
skenny <skenny@tchpc.tcd.ie>
2018-08-29 16:31:23 +0100

    #1815 remove asset actor, create new model and remove asset behaviour concern

--- config/initializers/batch_files_support.rb
+++ config/initializers/batch_files_support.rb
@@ -2,14 +2,13 @@ require 'dri/model_support/files'
 require 'validators'
 
 DRI::ModelSupport::Files.module_eval do
-  include DRI::AssetBehaviour
 
   def add_file(file, dsid='content', original_file_name)
-    @mime_type = Validators.file_type(file.path)
+    mime_type = Validators.file_type(file.path)
     pass_validation = false
 
     begin
-      pass_validation = Validators.validate_file(file.path, @mime_type)
+      pass_validation = Validators.validate_file(file.path, mime_type)
     rescue Exception => e
       Rails.logger.error "Error validating file: #{e.message}"
       return false
@@ -17,19 +16,16 @@ DRI::ModelSupport::Files.module_eval do
 
     return false unless pass_validation
 
-    @generic_file = DRI::GenericFile.new(id: DRI::Noid::Service.new.mint)
-    @generic_file.batch = self
-    filename = "#{@generic_file.id}_#{original_file_name}"
-      
+    generic_file = DRI::GenericFile.new(id: DRI::Noid::Service.new.mint)
+    generic_file.batch = self
+    filename = "#{generic_file.id}_#{original_file_name}"
+
     # Apply depositor metadata, other permissions currently unused for generic files
-    ingest_user = UserGroup::User.find_by_email(self.depositor)    
-    @generic_file.apply_depositor_metadata(self.depositor)
+    ingest_user = UserGroup::User.find_by_email(self.depositor)
+    generic_file.apply_depositor_metadata(self.depositor)
 
-    @actor = DRI::Asset::Actor.new(@generic_file, ingest_user)
-
-    current_version = self.object_version || '1'
-    object_version = current_version.next
-    self.object_version = object_version
+    self.object_version ||= '1'
+    self.increment_version
 
     # Update object version
     begin
@@ -38,21 +34,28 @@ DRI::ModelSupport::Files.module_eval do
       logger.error "Could not update object version number for #{self.id} to version #{object_version}"
       raise Exceptions::InternalError
     end
-    
-    create_local_file(self, file, dsid, nil, filename)
-        
+
+    LocalFile.build_local_file(
+      object: self,
+      generic_file: generic_file,
+      data: file,
+      datastream: dsid,
+      opts: { filename: filename }
+    )
+
     preservation = Preservation::Preservator.new(self)
     preservation.preserve_assets([filename],[])
-    
+
     url = Rails.application.routes.url_helpers.url_for(
       controller: 'assets',
       action: 'download',
       object_id: self.id,
-      id: @generic_file.id,
+      id: generic_file.id,
       version: object_version
     )
 
-    if @actor.create_external_content(URI.escape(url), dsid, filename)
+    file_content = GenericFileContent.new(user: ingest_user, generic_file: generic_file)
+    if file_content.external_content(URI.escape(url), filename, dsid)
       true
     else
       Rails.logger.error "Error saving file: #{e.message}"
--- spec/controllers/assets_spec.rb
+++ spec/controllers/assets_spec.rb
@@ -6,7 +6,7 @@ describe AssetsController do
   before(:each) do
     @tmp_upload_dir = Dir.mktmpdir
     @tmp_assets_dir = Dir.mktmpdir
-    
+
     Settings.dri.uploads = @tmp_upload_dir
     Settings.dri.files = @tmp_assets_dir
 
@@ -14,21 +14,21 @@ describe AssetsController do
     sign_in @login_user
 
     @collection = FactoryBot.create(:collection)
-   
-    @object = FactoryBot.create(:sound) 
+
+    @object = FactoryBot.create(:sound)
     @object[:status] = "draft"
     @object.save
 
     @collection.governed_items << @object
 
-    @collection.save    
+    @collection.save
   end
 
   after(:each) do
     @collection.delete
     @login_user.delete
-    FileUtils.remove_dir(@tmp_upload_dir, :force => true)
-    FileUtils.remove_dir(@tmp_assets_dir, :force => true)
+    FileUtils.remove_dir(@tmp_upload_dir, force: true)
+    FileUtils.remove_dir(@tmp_assets_dir, force: true)
   end
 
   describe 'show' do
@@ -38,7 +38,7 @@ describe AssetsController do
       generic_file.batch = @object
       generic_file.apply_depositor_metadata(@login_user.email)
       generic_file.save
-      
+
       get :show, { object_id: @object.id, id: generic_file.id, surrogate: 'thumbnail' }
       expect(response.status).to eq(404)
     end
@@ -48,27 +48,27 @@ describe AssetsController do
   describe 'create' do
 
     it 'should create an asset from a local file' do
-      allow_any_instance_of(DRI::Asset::Actor).to receive(:create_external_content)
+      allow_any_instance_of(GenericFileContent).to receive(:external_content)
 
       FileUtils.cp(File.join(fixture_path, "SAMPLEA.mp3"), File.join(@tmp_upload_dir, "SAMPLEA.mp3"))
-      options = { :file_name => "SAMPLEA.mp3" }      
+      options = { :file_name => "SAMPLEA.mp3" }
       post :create, { :object_id => @object.id, :local_file => "SAMPLEA.mp3", :file_name => "SAMPLEA.mp3" }
-       
+
       expect(Dir.glob("#{@tmp_assets_dir}/**/*_SAMPLEA.mp3")).not_to be_empty
     end
 
     it 'should create a valid aip' do
-      allow_any_instance_of(DRI::Asset::Actor).to receive(:create_external_content)
+      allow_any_instance_of(GenericFileContent).to receive(:external_content)
 
       FileUtils.cp(File.join(fixture_path, "SAMPLEA.mp3"), File.join(@tmp_upload_dir, "SAMPLEA.mp3"))
-      options = { :file_name => "SAMPLEA.mp3" }      
+      options = { :file_name => "SAMPLEA.mp3" }
       post :create, { :object_id => @object.id, :local_file => "SAMPLEA.mp3", :file_name => "SAMPLEA.mp3" }
-       
+
       expect(aip_valid?(@object.id, 2)).to be true
     end
 
     it 'should create an asset from an upload' do
-      allow_any_instance_of(DRI::Asset::Actor).to receive(:create_external_content)
+      allow_any_instance_of(GenericFileContent).to receive(:external_content)
 
       @uploaded = Rack::Test::UploadedFile.new(File.join(fixture_path, "SAMPLEA.mp3"), "audio/mp3")
       post :create, { :object_id => @object.id, :Filedata => @uploaded }
@@ -94,7 +94,7 @@ describe AssetsController do
 
       DataciteDoi.create(object_id: @object.id)
 
-      expect_any_instance_of(DRI::Asset::Actor).to receive(:create_external_content).and_return(true)
+      expect_any_instance_of(GenericFileContent).to receive(:external_content).and_return(true)
 
       expect(DRI.queue).to receive(:push).with(an_instance_of(MintDoiJob)).once
       @uploaded = Rack::Test::UploadedFile.new(File.join(fixture_path, "SAMPLEA.mp3"), "audio/mp3")
@@ -106,10 +106,9 @@ describe AssetsController do
 
    end
 
-   describe 'update' do  
+   describe 'update' do
     it 'should create a new version' do
-      allow_any_instance_of(DRI::Asset::Actor).to receive(:create_external_content)
-      allow_any_instance_of(DRI::Asset::Actor).to receive(:update_external_content)
+      allow_any_instance_of(GenericFileContent).to receive(:external_content)
 
       generic_file = DRI::GenericFile.new(id: ActiveFedora::Noid::Service.new.mint)
       generic_file.batch = @object
@@ -119,7 +118,7 @@ describe AssetsController do
       options[:mime_type] = "audio/mp3"
       options[:file_name] = "#{generic_file.id}_SAMPLEA.mp3"
       options[:batch_id] = @object.id
-       
+
       uploaded = Rack::Test::UploadedFile.new(File.join(fixture_path, "SAMPLEA.mp3"), "audio/mp3")
       file.add_file uploaded, options
       file.save
@@ -141,13 +140,12 @@ describe AssetsController do
 
       @uploaded = Rack::Test::UploadedFile.new(File.join(fixture_path, "sample_image.jpeg"), "image/jpeg")
       put :update, { :object_id => @object.id, :id => file_id, :Filedata => @uploaded }
-       
+
       expect(aip_valid?(@object.id, 3)).to be true
     end
 
     it 'should create a new version from a local file' do
-      allow_any_instance_of(DRI::Asset::Actor).to receive(:create_external_content)
-      allow_any_instance_of(DRI::Asset::Actor).to receive(:update_external_content)
+      allow_any_instance_of(GenericFileContent).to receive(:external_content)
 
       FileUtils.cp(File.join(fixture_path, "SAMPLEA.mp3"), File.join(@tmp_upload_dir, "SAMPLEA.mp3"))
 
@@ -170,8 +168,7 @@ describe AssetsController do
     end
 
     it 'should mint a doi when an asset is modified' do
-      allow_any_instance_of(DRI::Asset::Actor).to receive(:create_external_content)
-      allow_any_instance_of(DRI::Asset::Actor).to receive(:update_external_content).and_return(true)
+      allow_any_instance_of(GenericFileContent).to receive(:external_content).and_return(true)
 
       stub_const(
         'DoiConfig',
@@ -207,7 +204,7 @@ describe AssetsController do
 
       expect(DRI.queue).to receive(:push).with(an_instance_of(MintDoiJob)).once
       put :update, { :object_id => @object.id, :id => file_id, :local_file => "SAMPLEA.mp3", :file_name => "SAMPLEA.mp3" }
-       
+
       DataciteDoi.where(object_id: @object.id).each { |d| d.delete }
       Settings.doi.enable = false
     end
@@ -215,10 +212,10 @@ describe AssetsController do
   end
 
   describe 'destroy' do
-    
+
     it 'should delete a file' do
-      allow_any_instance_of(DRI::Asset::Actor).to receive(:create_external_content)
-      
+      allow_any_instance_of(GenericFileContent).to receive(:external_content)
+
       generic_file = DRI::GenericFile.new(id: ActiveFedora::Noid::Service.new.mint)
       generic_file.batch = @object
       generic_file.apply_depositor_metadata('test@test.com')
@@ -227,7 +224,7 @@ describe AssetsController do
       options[:mime_type] = "audio/mp3"
       options[:file_name] = "SAMPLEA.mp3"
       options[:batch_id] = @object.id
-       
+
       uploaded = Rack::Test::UploadedFile.new(File.join(fixture_path, "SAMPLEA.mp3"), "audio/mp3")
       file.add_file uploaded, options
       file.save
@@ -237,15 +234,15 @@ describe AssetsController do
       expect {
         delete :destroy, object_id: @object.id, id: file_id
       }.to change { ActiveFedora::Base.exists?(file_id) }.from(true).to(false)
-      
+
     end
 
   end
 
   describe 'download' do
-  
+
     it "should be possible to download the master asset" do
-      allow_any_instance_of(DRI::Asset::Actor).to receive(:create_external_content)
+      allow_any_instance_of(GenericFileContent).to receive(:external_content)
 
       @object.master_file_access = 'public'
       @object.edit_users_string = @login_user.email
@@ -271,14 +268,14 @@ describe AssetsController do
       get :download, id: file_id, object_id: @object.id, type: 'masterfile'
       expect(response.status).to eq(200)
       expect(response.header['Content-Type']).to eq('audio/mp3')
-      expect(response.header['Content-Length']).to eq("#{File.size(File.join(fixture_path, "SAMPLEA.mp3"))}")      
+      expect(response.header['Content-Length']).to eq("#{File.size(File.join(fixture_path, "SAMPLEA.mp3"))}")
     end
   end
 
   describe 'list_assets' do
 
     before(:each) do
-      allow_any_instance_of(DRI::Asset::Actor).to receive(:create_external_content)
+      allow_any_instance_of(GenericFileContent).to receive(:external_content)
 
       @object.master_file_access = 'public'
       @object.edit_users_string = @login_user.email
@@ -314,7 +311,7 @@ describe AssetsController do
       request.env["HTTP_ACCEPT"] = 'application/json'
       post :list_assets, objects: [ { "pid" => "#{@object.id}" } ]
       list = controller.instance_variable_get(:@list)
-      
+
       expect(list.first).to include('files')
       expect(list.first['files'].first).to include('masterfile')
       expect(list.first['files'].first).to include('mp3')
@@ -339,7 +336,7 @@ describe AssetsController do
       request.env["HTTP_ACCEPT"] = 'application/json'
       post :list_assets, objects: [ { "pid" => "#{@object.id}" } ]
       list = controller.instance_variable_get(:@list)
-      
+
       expect(list.first).to include('files')
       expect(list.first['files'].count).to be 1
     end
@@ -357,7 +354,7 @@ describe AssetsController do
 
         @login_user = FactoryBot.create(:admin)
         sign_in @login_user
-        @object = FactoryBot.create(:sound) 
+        @object = FactoryBot.create(:sound)
 
         request.env["HTTP_REFERER"] = catalog_index_path
       end
@@ -365,7 +362,7 @@ describe AssetsController do
       after(:each) do
         @object.delete if ActiveFedora::Base.exists?(@object.id)
         @login_user.delete
-        
+
         FileUtils.remove_dir(@tmp_assets_dir, force: true)
         Settings.reload_from_files(
           Rails.root.join("config", "settings.yml").to_s
@@ -375,11 +372,11 @@ describe AssetsController do
     describe 'create' do
 
       it 'should not create an asset' do
-        allow_any_instance_of(DRI::Asset::Actor).to receive(:create_external_content)
+        allow_any_instance_of(GenericFileContent).to receive(:external_content)
 
         @uploaded = Rack::Test::UploadedFile.new(File.join(fixture_path, "SAMPLEA.mp3"), "audio/mp3")
         post :create, { :object_id => @object.id, :Filedata => @uploaded }
-        
+
         expect(flash[:error]).to be_present
       end
     end
