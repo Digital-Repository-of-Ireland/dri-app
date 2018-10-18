d756a39bf7529352205911b8ae9b6b879172d249
conors_nli <conor.sheehan.2@ucdconnect.ie>
2018-10-18 16:02:10 +0100

    change vocab select working

--- app/assets/javascripts/dri/autocomplete_vocabs.js
+++ app/assets/javascripts/dri/autocomplete_vocabs.js
@@ -1,15 +1,12 @@
 function setOclcAutocomplete(selector='.dri-textfield') {
-  var endpoint = $('#toggle_vocab').find(':selected').val();
+  var endpoint = $('#choose_vocab').find(':selected').val();
   if (endpoint == 'na') { 
-    $(selector).autocomplete({source: []});
+    $(selector).autocomplete({source: []}); // turn autocomplete off
     return false; 
   }
-  console.log(endpoint);
   $(selector).autocomplete({
     source: function (request, response) {
       $.ajax({
-        // /qa/search/loc/subjects?q=
-        // /qa/search/assign_fast/all?q=
         url: endpoint + request.term + '&maximumRecords=5',
         type: 'GET',
         dataType: 'json',
@@ -20,60 +17,42 @@ function setOclcAutocomplete(selector='.dri-textfield') {
       });
     },
     autoFocus: true
-  })
-}
-
-function addVocabToggle(selector='.dri-textfield') {
-  var to_insert_beside = $(selector)[0]
-  if (to_insert_beside === undefined) {
-    return false;
-  } else {
-    var toggle_vocab = document.createElement('select');
-    toggle_vocab.id = 'toggle_vocab'
-    toggle_vocab.onchange = setOclcAutocomplete
-    var options = [
-      ["Library of Congress", "/qa/search/loc/subjects?q="],
-      ["OCLC FAST", "/qa/search/assign_fast/all?q="],
-      ["No authority (disable autocomplete)", "na"]
-    ];
-
-    options.forEach(function(arr){
-      var option = document.createElement('option');
-      option.text = arr[0];
-      option.value = arr[1];
-      toggle_vocab.appendChild(option);
-    });
-
-    // let m = new Map([
-    //   ["Library of Congress", "/qa/search/loc/subjects?q="],
-    //   ["OCLC FAST", "/qa/search/assign_fast/all?q="],
-    //   ["No authority (disable autocomplete)", "na"]
-    // ]);
-
-    // m.forEach((key, value) => {
-    //   let option = document.createElement('option');
-    //   option.text = key;
-    //   option.value = value;
-    //   toggle_vocab.appendChild(option);
-    // });
-
-    to_insert_beside.before(
-      // '\
-      // <select>\
-      //    <option value="/qa/search/loc/subjects?q=">Library of Congress</option>\
-      //    <option value="/qa/search/assign_fast/all?q=">OCLC FAST</option>\
-      //    <option value="na">No authority (disable autocomplete)</option>\
-      // </select>\
-      // '
-      toggle_vocab,
-      document.createElement('br')
-    );
-  }
+  });
   return true;
 }
 
-$(document).ready(function($) {
-  if (addVocabToggle()) {
-    setOclcAutocomplete();
+function addChooseVocab(selector='.dri-textfield') {
+  var to_insert_beside = $(selector)[0];
+  if (to_insert_beside === undefined) {
+    return false;
   }
+  var choose_vocab = document.createElement('select');
+  choose_vocab.id = 'choose_vocab'
+  var options = [
+    ["Library of Congress", "/qa/search/loc/subjects?q="],
+    ["OCLC FAST", "/qa/search/assign_fast/all?q="],
+    ["No authority (disable autocomplete)", "na"]
+  ];
+
+  options.forEach(function(arr){
+    var option = document.createElement('option');
+    option.text = arr[0];
+    option.value = arr[1];
+    choose_vocab.appendChild(option);
+  });
+
+  to_insert_beside.before(
+    choose_vocab,
+    document.createElement('br')
+  );
+  return true;
+}
+
+// wait til doc is ready
+$(function () {
+  addChooseVocab();
+  setOclcAutocomplete(); // set autocomplete when page loads
+  $('#choose_vocab').change(function() {
+    setOclcAutocomplete(); // update autocomplete using value of select option
+  });
 });
