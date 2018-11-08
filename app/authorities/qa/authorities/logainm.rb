cf64cd1776b61c62fd06c29d686e24655e63ce40
conors_nli <conor.sheehan.2@ucdconnect.ie>
2018-11-08 15:22:24 +0000

    add comments about ruby sparql client still not able to use fully due to redirect issue with unesco and json formatting results

--- app/authorities/qa/authorities/logainm.rb
+++ app/authorities/qa/authorities/logainm.rb
@@ -1,10 +1,12 @@
 require 'httparty'
+# require 'sparql/client'
 
 module Qa::Authorities
   class Logainm < Qa::Authorities::Base
     def search(_q)
       # limit is 100, timeout after 5 seconds
       # regex query so will be slow
+      # but allows for non-exact matches e.g. Henry_Street_(Dublin)
 
       # SELECT distinct ?s ?p ?o
       # WHERE {
@@ -22,6 +24,30 @@ module Qa::Authorities
         }
       end
     end
+
+    # # Issues with sparql client results as json
+    # # Also sparql client doesn't work with unesco api
+    # # SPARQL::Client::ServerError: Infinite redirect at http://vocabularies.unesco.org/sparql
+    # # can use .to_query to build uri instead of using sparql client to make request
+    # def search(_q)
+    #   sparql_endpoint = 'http://data.logainm.ie/sparql'
+    #   query = SPARQL::Client.new(sparql_endpoint)
+    #       .select(distinct: true)
+    #       .where([:s, :p, :o])
+    #       .filter("regex(?o, '#{_q}', 'i')")
+    #       .limit(10)
+    #       .to_query('query')
+
+    #   url = "#{sparql_endpoint}?#{query}&format=application%2Fsparql-results%2Bjson&timeout=5000"
+    #   response = HTTParty.get(url)
+    #   json_response = JSON.parse(response.body, symbolize_names: true)
+    #   json_response[:results][:bindings].map do |h| 
+    #     {
+    #       id: h[:s][:value],
+    #       label: h[:o][:value],
+    #     }
+    #   end
+    # end
   end
 end
 
