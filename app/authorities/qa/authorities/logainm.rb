e3e4794c03d37ce03b8fb3b1669194c930d273cf
conors_nli <conor.sheehan.2@ucdconnect.ie>
2018-11-09 12:07:47 +0000

    logainm: use contains lcase instead of regex to speed up query further update var names in query

--- app/authorities/qa/authorities/logainm.rb
+++ app/authorities/qa/authorities/logainm.rb
@@ -7,24 +7,25 @@ module Qa::Authorities
       # # limit is 100, timeout after 5 seconds
       # # regex query so will be slow
       # # but allows for non-exact matches
-      # # only get names of places, so misses isReferencedBy etc
-      # # get min ranked link and group by so places are unique
-      # SELECT MIN(?link) ?place_name
+      # # use sample instead of group by since link itself doesn't matter 
+      # SELECT ?place_name (SAMPLE(?link) AS ?link)
       # WHERE {
-      #   ?link ?o ?place_name .
-      #   ?link foaf:name ?place_name
-      #   FILTER(regex(?place_name, "dublin", "i"))
-      # } GROUP BY ?place_name
+      #   ?link foaf:name ?place_name .
+      #   # FILTER (regex(?place_name, "henry", "i"))
+      #   FILTER (CONTAINS(LCASE(?place_name), "henry"))
+      # }
+      # LIMIT 50
 
-      url = "http://data.logainm.ie/sparql?default-graph-uri=&query=++++SELECT+MIN%28%3Flink%29+%3Fplace_name%0D%0A++++WHERE+%7B%0D%0A++++++%3Flink+%3Fo+%3Fplace_name+.%0D%0A++++++%3Flink+foaf%3Aname+%3Fplace_name%0D%0A++++++FILTER%28regex%28%3Fplace_name%2C+%22#{_q}%22%2C+%22i%22%29%29%0D%0A++++%7D+GROUP+BY+%3Fplace_name&format=application%2Fsparql-results%2Bjson"
+      url = "http://data.logainm.ie/sparql?default-graph-uri=&query=SELECT+%3Fplace_name+%28SAMPLE%28%3Flink%29+AS+%3Flink%29%0D%0A++++++WHERE+%7B%0D%0A++++++++%3Flink+foaf%3Aname+%3Fplace_name+.%0D%0A++++++++FILTER+%28CONTAINS%28LCASE%28%3Fplace_name%29%2C+%22#{_q}%22%29%29%0D%0A++++++%7D%0D%0A++++++LIMIT+50&format=application%2Fsparql-results%2Bjson"
+
       response = HTTParty.get(url)
       json_response = JSON.parse(response.body, symbolize_names: true)
       json_response[:results][:bindings].map do |h| 
         {
           # id: h[:s][:value],
           # label: h[:o][:value],
-          id: h[:place][:value],
-          label: h[:name][:value],
+          id: h[:link][:value],
+          label: h[:place_name][:value],
         }
       end
     end
@@ -42,7 +43,7 @@ module Qa::Authorities
     #       .limit(10)
     #       .to_query('query')
 
-    #   url = "#{sparql_endpoint}?#{query}&format=application%2Fsparql-results%2Bjson&timeout=5000"
+    #   url = "{sparql_endpoint}?#{query}&format=application%2Fsparql-results%2Bjson"
     #   response = HTTParty.get(url)
     #   json_response = JSON.parse(response.body, symbolize_names: true)
     #   json_response[:results][:bindings].map do |h| 
@@ -54,48 +55,3 @@ module Qa::Authorities
     # end
   end
 end
-
-# # find everything that is not a logainm/place
-# SELECT distinct ?s ?p ?o
-# WHERE {
-#   ?s ?p ?o .
-#   FILTER regex(?s, "^(?!http://data.logainm.ie/place).*$", "i")
-# }
-
-
-# find every logainm place with a similar name to locha
-# SELECT distinct ?s ?p ?o
-# WHERE {
-#   ?s ?p ?o .
-#   FILTER regex(?s, "^http://data.logainm.ie/place/(.*)$", "i")
-#   FILTER regex(?o, "locha", "i")
-# }
-
-# # find subject, predicate, object 
-# # where name of subject matches locha
-# PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-# SELECT ?s ?p ?o
-# WHERE {
-#   ?s ?p ?o .
-#   FILTER(?p = foaf:name)
-#   FILTER(regex(?o, "locha", "i"))
-# }
-# # produces data.logainm/place links, which are all dead :(
-
-# PREFIX dc: <http://purl.org/dc/terms/>
-# SELECT ?s ?p ?o
-# WHERE {
-#   ?s ?p ?o .
-#   FILTER(?p = dc:isReferencedBy)
-#   FILTER(regex(?o, "locha", "i"))
-# }
-
-# PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-# SELECT ?place ?name
-# WHERE {
-#   # find all places with name "An Clochar"@ga
-#   ?place foaf:name "An Clochar"@ga .
-#   # save foaf:name as new variable named ?name
-#   ?place foaf:name ?name .
-# }
-
