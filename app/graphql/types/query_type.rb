d0ca90b3af07e6483a1e7aa1a523253b8dacf985
Conor Sheehan <conor.sheehan.dev@gmail.com>
2019-03-12 13:18:35 +0000

    add query resolver for objects try ActiveFedora::SolrService. Returns array instead of ActiveFedora::Relation, so limit and offset functions woudl need to be changed

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,27 +7,9 @@ module Types
     field :all_collections, [CollectionType], null: false,
           description: "All published collections", function: Resolvers::CollectionsSearch
     field :all_objects, [ObjectType], null: false,
-          description: "All published objects"
+          description: "All published objects", function: Resolvers::ObjectsSearch
 
     # could refactor to use enum option to specify type?
     # .e.g search(type:collection) instead of CollectionsSearch
-
-    def all_objects
-      # # no object field, closest is !collection? && batch?
-      # # see app/models/solr_document.rb#object?
-      # # 
-      # # can't rely on object_type since it's set by user input
-      # # what about file_type? (file_type_sim) method alias object#type
-      # # what about has_model?
-      # # catalog and my_collections controllers use not collection and exclude generic files
-      # # see app/controllers/concerns/dri/catalog.rb
-
-      collection_field = Solr::SchemaFields.facet('is_collection')
-      DRI::QualifiedDublinCore.where(
-        "#{collection_field}": 'false',
-        # "#{generic_file_field}": 'DRI::GenericFile'
-        status: 'published'
-      )
-    end
   end
 end
