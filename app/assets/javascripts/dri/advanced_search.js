8af6796bb6721cf2584b6c5a16ef498b01bc5035
Conor Sheehan <conor.sheehan.dev@gmail.com>
2019-03-25 11:06:23 +0000

    change params based on tab selection
    if no tab is clicked look for mode param in url, otherwise default to collections
    needed to explicitly add show_sub hidden tag, mode tag was already in advanced search form

--- app/assets/javascripts/dri/advanced_search.js
+++ app/assets/javascripts/dri/advanced_search.js
@@ -23,12 +23,18 @@ $(document).ready(function(){
 
   $('#dri_browse_sort_tabs_collections_id_no_reload a').on('click', function() {
     $('#dri_browse_sort_tabs_collections_id_no_reload a').addClass('selected');
+    $('.advanced input[name=mode]').val('collections');
+    $('.advanced input[name=show_subs]').val(false);
+
     $('#dri_browse_sort_tabs_sub_collections_id_no_reload a').removeClass('selected');
     $('#dri_browse_sort_tabs_objects_id_no_reload a').removeClass('selected');
   });
 
   $('#dri_browse_sort_tabs_objects_id_no_reload a').on('click', function() {
     $('#dri_browse_sort_tabs_objects_id_no_reload a').addClass('selected');
+    $('.advanced input[name=mode]').val('objects');
+    $('.advanced input[name=show_subs]').val(false);
+
     $('#dri_browse_sort_tabs_collections_id_no_reload a').removeClass('selected');
     $('#dri_browse_sort_tabs_sub_collections_id_no_reload a').removeClass('selected');
   });
@@ -36,6 +42,9 @@ $(document).ready(function(){
   // sub-collections special case
   $('#dri_browse_sort_tabs_sub_collections_id_no_reload a').on('click', function() {
     $('#dri_browse_sort_tabs_sub_collections_id_no_reload a').addClass('selected');
+    $('.advanced input[name=mode]').val('collections');
+    $('.advanced input[name=show_subs]').val(true);
+
     $('#dri_browse_sort_tabs_collections_id_no_reload a').addClass('selected');
     $('#dri_browse_sort_tabs_objects_id_no_reload a').removeClass('selected');
   });
@@ -44,7 +53,7 @@ $(document).ready(function(){
     return $(class_name).hasClass('selected') != true;
   }
 
-  // drop support for ie <= 9 and use Array.every
+  // drop support for ie < 9 and use Array.every instead
   function array_all(arr, elem_callback) {
     $.each(arr, function(i, el) {
       if (elem_callback(el) != true) {
@@ -55,15 +64,26 @@ $(document).ready(function(){
     return true;
   }
 
-  // if none are selected, select collections
-  class_names = [
+  // if dropping support for ie, use urlsearchparams instead
+  function get_url_param(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return (results == null) ? null : results[1] || 0;
+  }
+
+  var class_names = [
     '#dri_browse_sort_tabs_collections_id_no_reload a',
     '#dri_browse_sort_tabs_objects_id_no_reload a',
     '#dri_browse_sort_tabs_sub_collections_id_no_reload a'
   ];
 
+  // if none are selected, check url params, select collections by default
   if (array_all(class_names, not_selected)) {
-    $('#dri_browse_sort_tabs_collections_id_no_reload a').click();
+    var current_mode = get_url_param('mode');
+    if (current_mode) { 
+      $('#dri_browse_sort_tabs_' + current_mode + '_id_no_reload a').click();
+    } else {
+      $('#dri_browse_sort_tabs_collections_id_no_reload a').click();
+    }
   }
 
 });
