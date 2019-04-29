c52f629a6cf2c9fd216760ec532d335f1755e585
Conor Sheehan <conor.sheehan.dev@gmail.com>
2019-04-29 14:28:37 +0100

    refactor authority view logic into presenter.

---
+++ app/presenters/dri/authority_presenter.rb
@@ -0,0 +1,24 @@
+module DRI
+  class AuthorityPresenter
+    def self.vocabs
+      [
+        { name: "LOC Subject Headings", endpoint: "/qa/search/loc/subjects?q=" },
+        { name: "LOC Names", endpoint: "/qa/search/loc/names?q=" },
+        { name: "Getty Art and Architecture", endpoint: "/qa/search/getty/aat?q=" },
+        { name: "Hasset", endpoint: "/qa/search/hasset/subjects?q=" },
+        { name: "Logainm", endpoint: "/qa/search/logainm/subjects?q=" },
+        { name: "Nuts3", endpoint: "/qa/search/nuts3/subjects?q=" },
+        { name: "OCLC FAST", endpoint: "/qa/search/assign_fast/all?q=" },
+        { name: "Unesco", endpoint: "/qa/search/unesco/subjects?q=" },
+        { name: "Disable", endpoint: "na" }
+      ]
+    end
+
+    def self.viewable_vocabs
+      # remove local authorities if they are empty
+      vocabs = self.vocabs
+      vocabs.reject! { |h| h[:name] == 'Nuts3' } if Qa::Authorities::Nuts3.new.empty?
+      vocabs.reject! { |h| h[:name] == 'Hasset' } if Qa::Authorities::Hasset.new.empty?
+    end
+  end
+end
