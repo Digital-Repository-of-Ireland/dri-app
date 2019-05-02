953c66899f5d6325c1ab4477e9812afb5734c606
Stuart Kenny <skenny@tchpc.tcd.ie>
2019-05-02 21:20:53 +0100

    use add_foreign_key

--- db/migrate/20190417125610_create_qa_local_authority_entries.rb
+++ db/migrate/20190417125610_create_qa_local_authority_entries.rb
@@ -1,12 +1,13 @@
 class CreateQaLocalAuthorityEntries < ActiveRecord::Migration
   def change
     create_table :qa_local_authority_entries do |t|
-      t.references :local_authority, foreign_key: { to_table: :qa_local_authorities }, index: true
+      t.references :local_authority, index: true
       t.string :label
       t.string :uri
 
       t.timestamps null: false
     end
     add_index :qa_local_authority_entries, :uri, unique: true
+    add_foreign_key :qa_local_authority_entries, :qa_local_authorities, column: :local_authority_id
   end
 end
