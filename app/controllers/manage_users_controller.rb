bd628c442c46e11ed6762e0bbdca0d7c98695845
Stuart Kenny <skenny@tchpc.tcd.ie>
2019-05-30 13:15:57 +0100

    add link to assign organisational managers for admins

--- app/controllers/manage_users_controller.rb
+++ app/controllers/manage_users_controller.rb
@@ -3,10 +3,11 @@ class ManageUsersController < ApplicationController
     if signed_in? && (current_user.is_admin? || current_user.is_om?)
       user = UserGroup::User.find_by_email(params[:user].strip)
       if user.present?
-        group_id = UserGroup::Group.find_by_name('cm').id
-        membership = user.join_group(group_id)
-        membership.approved_by = current_user.id
-        membership.save
+        groups = ['cm']
+        groups << 'om' if current_user.is_admin? && assign_om?
+
+        add_groups_to_user(user, groups)
+
         flash[:notice] = t('dri.flash.notice.manager_user_created', email: params[:user])
       else
         flash[:error] = t('dri.flash.error.manager_user_invalid', email: params[:user])
@@ -19,4 +20,18 @@ class ManageUsersController < ApplicationController
       format.html { render action: 'new' }
     end
   end
+
+  private
+    def assign_om?
+       params[:type].present? && params[:type] == 'om'
+    end
+
+    def add_groups_to_user(user, groups)
+      groups.each do |group|
+        group_id = UserGroup::Group.find_by_name(group).id
+        membership = user.join_group(group_id)
+        membership.approved_by = current_user.id
+        membership.save
+      end
+    end
 end
