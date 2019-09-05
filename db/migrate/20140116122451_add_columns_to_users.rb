c2dc21f2088de065adf9d6b9edb7632d1a390874
Stuart Kenny <skenny@tchpc.tcd.ie>
2019-09-05 09:46:56 +0100

    revert copied migrations

--- db/migrate/20140116122451_add_columns_to_users.rb
+++ db/migrate/20140116122451_add_columns_to_users.rb
@@ -1,4 +1,4 @@
-class AddColumnsToUsers < ActiveRecord::Migration
+class AddColumnsToUsers < ActiveRecord::Migration[4.2]
   def change
     add_column :user_group_users, :provider, :string
     add_column :user_group_users, :uid, :string
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_165357) do
+ActiveRecord::Schema.define(version: 2019_07_22_154607) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -254,8 +254,6 @@ ActiveRecord::Schema.define(version: 2019_08_28_165357) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string "provider"
-    t.string "uid"
     t.index ["authentication_token"], name: "index_user_group_users_on_authentication_token", unique: true
     t.index ["confirmation_token"], name: "index_user_group_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_user_group_users_on_email", unique: true
