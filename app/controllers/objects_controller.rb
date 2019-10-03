498d46ca8a72c6ea81f8e6d231b842c70caa0261
Stuart Kenny <skenny@tchpc.tcd.ie>
2019-10-03 14:41:52 +0100

    extract api methods to single controller

--- app/controllers/objects_controller.rb
+++ app/controllers/objects_controller.rb
@@ -9,8 +9,8 @@ class ObjectsController < BaseObjectsController
 
   before_action :authenticate_user_from_token!, except: [:show, :citation]
   before_action :authenticate_user!, except: [:show, :citation]
-  before_action :read_only, except: [:index, :show, :citation, :related, :retrieve]
-  before_action ->(id=params[:id]) { locked(id) }, except: [:index, :show, :citation, :related, :new, :create, :retrieve]
+  before_action :read_only, except: [:show, :citation, :related, :retrieve]
+  before_action ->(id=params[:id]) { locked(id) }, except: [:show, :citation, :related, :new, :create, :retrieve]
 
   # Displays the New Object form
   #
@@ -63,10 +63,8 @@ class ObjectsController < BaseObjectsController
       format.endnote { render plain: @object.export_as_endnote, layout: false }
       format.json do
         json = @object.as_json
-        # solr_doc = SolrDocument.new(@object.to_solr)
-        # # intermittent issue using to_solr:  A JSON text must at least contain two octets!
         solr_query = ActiveFedora::SolrQueryBuilder.construct_query_for_ids([@object.id])
-        solr_doc = SolrDocument.new(Solr::Query.new(solr_query).first)
+        solr_doc = Solr::Query.new(solr_query).first
 
         json['licence'] = DRI::Formatters::Json.licence(solr_doc)
         json['doi'] = DRI::Formatters::Json.dois(solr_doc)
@@ -239,82 +237,6 @@ class ObjectsController < BaseObjectsController
     end
   end
 
-  def index
-    @list = []
-    if params[:objects].present?
-      object_ids = params[:objects].map { |o| o.values.first }
-    else
-      err_msg = 'No objects in params'
-      logger.error "#{err_msg} #{params.inspect}"
-      raise DRI::Exceptions::BadRequest
-    end
-
-    solr_query = ActiveFedora::SolrQueryBuilder.construct_query_for_ids(object_ids)
-    results = Solr::Query.new(solr_query)
-
-    results.each do |solr_doc|
-      next unless solr_doc.published? || (current_user.is_admin? || can?(:edit, solr_doc))
-
-      item = Rails.cache.fetch("get_objects-#{solr_doc.id}-#{solr_doc['system_modified_dtsi']}") do
-               solr_doc.extract_metadata(params[:metadata])
-             end
-
-      item['metadata']['licence'] = DRI::Formatters::Json.licence(solr_doc)
-      item['metadata']['doi'] = DRI::Formatters::Json.dois(solr_doc)
-      item['metadata']['related_objects'] = solr_doc.object_relationships_as_json
-
-      item.merge!(find_assets_and_surrogates(solr_doc))
-      @list << item
-    end
-
-    raise DRI::Exceptions::NotFound if @list.empty?
-
-    respond_to do |format|
-      format.json { render(json: @list) }
-    end
-  end
-
-  def related
-    enforce_permissions!('show_digital_object', params[:object])
-
-    count = if params[:count].present? && numeric?(params[:count])
-              params[:count]
-            else
-              3
-            end
-
-    if params[:object].present?
-      solr_query = ActiveFedora::SolrQueryBuilder.construct_query_for_ids([params[:object]])
-      result = ActiveFedora::SolrService.instance.conn.get(
-        'select',
-        params: {
-          q: solr_query, qt: 'standard',
-          fq: "#{ActiveFedora.index_field_mapper.solr_name('is_collection', :stored_searchable, type: :string)}:false
-               AND #{ActiveFedora.index_field_mapper.solr_name('status', :stored_searchable, type: :symbol)}:published",
-          mlt: 'true',
-          :'mlt.fl' => "#{ActiveFedora.index_field_mapper.solr_name('subject', :stored_searchable, type: :string)},
-                        #{ActiveFedora.index_field_mapper.solr_name('subject', :stored_searchable, type: :string)}",
-          :'mlt.count' => count,
-          fl: 'id,score',
-          :'mlt.match.include' => 'false'
-        }
-      )
-    end
-
-    # TODO: fixme!
-    @related = []
-    if result && result['moreLikeThis'] && result['moreLikeThis'].first &&
-       result['moreLikeThis'].first[1] && result['moreLikeThis'].first[1]['docs']
-      result['moreLikeThis'].first[1]['docs'].each do |item|
-        @related << item
-      end
-    end
-
-    respond_to do |format|
-      format.json {}
-    end
-  end
-
   def retrieve
     id = params[:id]
     begin
@@ -324,7 +246,7 @@ class ObjectsController < BaseObjectsController
     end
 
     if object.present?
-      if (can? :read, object)
+      if can?(:read, object)
         if File.file?(File.join(Settings.dri.downloads, params[:archive]))
           response.headers['Content-Length'] = File.size?(params[:archive]).to_s
           send_file File.join(Settings.dri.downloads, params[:archive]),
@@ -437,35 +359,6 @@ class ObjectsController < BaseObjectsController
 
       ActiveFedora::SolrService.delete(id)
       flash[:notice] = t('dri.flash.notice.object_deleted')
-    end
-
-    def find_assets_and_surrogates(doc)
-      item = {}
-      item['files'] = []
-
-      # Get files
-      if can? :read, doc
-        files = doc.assets
-
-        files.each do |file_doc|
-          file_list = {}
-
-          if (doc.read_master? && can?(:read, doc)) || can?(:edit, doc)
-            url = url_for(file_download_url(doc.id, file_doc.id))
-            file_list['masterfile'] = url
-          end
-
-          timeout = 60 * 60 * 24 * 7
-          surrogates = doc.surrogates(file_doc.id, timeout)
-          surrogates.each do |file, loc|
-            file_list[file] = loc
-          end
-
-          item['files'].push(file_list)
-        end
-      end
-
-      item
     end
 
     def metadata_standard
