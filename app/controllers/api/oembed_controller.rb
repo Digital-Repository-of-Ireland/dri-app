93576273d10cea39e3538c5be894c7dde69ebe1f
Stuart Kenny <skenny@tchpc.tcd.ie>
2020-11-18 17:47:37 +0000

    Formatting and set content type changes
    Change the check for authorized access.

--- app/controllers/api/oembed_controller.rb
+++ app/controllers/api/oembed_controller.rb
@@ -1,88 +1,82 @@
 module Api
-   require 'solr/query'
-   class OembedController < ApplicationController
+  require 'solr/query'
+
+  class OembedController < ApplicationController
     include Blacklight::AccessControls::Catalog
-    
-   
-     before_action :set_headers
-     
-      def show  
-        
-        url = params.fetch(:url)
-        resource_url = URI.parse(url)
-        # Extract the resource ID from the path
-        resource_id = resource_url.path.match(%r|/catalog/([^/]+)|)
-        resource_id &&= resource_id[1]
-       
-        doc = SolrDocument.find(resource_id)
 
-        read_master = doc.read_master? ? 'public' : 'private'
-        
-        if read_master.include? 'private'
-           raise DRI::Exceptions::Unauthorized
-        end  
-        
-        type =   DRI::Formatters::EDM.edm_type(doc["type_tesim"])
-        assets = doc.assets(with_preservation: true, ordered: false)
+    before_action :set_headers
 
-        mainfile = DRI::Formatters::EDM.mainfile_for_type(type, assets)
+    def show
+      url = params.fetch(:url)
+      resource_url = URI.parse(url)
+      # Extract the resource ID from the path
+      resource_id = resource_url.path.match(%r|/catalog/([^/]+)|)
+      resource_id &&= resource_id[1]
+      raise DRI::Exceptions::BadRequest unless resource_id
 
-         if mainfile['file_type_tesim'].include? "3d"
-          embed_url = (URI.parse(root_url))+file_download_path(doc.id, mainfile.id, type: 'masterfile') 
-         end 
-       
-        raise DRI::Exceptions::NotFound if embed_url.nil?
+      doc = SolrDocument.find(resource_id)
+      raise DRI::Exceptions::Unauthorized unless can_view?(doc)
 
-        resource_title = doc['title_tesim']
-        
-        # Build up a JSON response with the required attributes
-        # See "2.3.4. Response parameters" at https://oembed.com/
-        @response = {
-          type: 'rich',
-          version: '1.0',
-          title: resource_title[0], 
-          provider_name: 'DRI: Digital Repository of Ireland',
-          provider_url: 'https://repository.dri.ie/',
-        
-          # not sure if this width and height is correct 
-          width: 500,
-          height: 500,
+      type =   DRI::Formatters::EDM.edm_type(doc["type_tesim"])
+      assets = doc.assets(with_preservation: true, ordered: false)
 
-          # Embedding url 
-          
-          html: <<-HTML
-          
-          <iframe src = \\"#{embed_url}\\" width=\\"500\\" height=\\"500\\">
+      mainfile = DRI::Formatters::EDM.mainfile_for_type(type, assets)
 
-          </iframe> 
-
-          HTML
-        }   
-        
-        respond_to do |format|         
-          if @response
-           format.json { render(json: @response)} 
-           format.xml  { render :xml => @response}              
-           else  
-           format.All   { raise DRI::Exceptions::NotImplemented }
-          end
-        end
-
-  
-     end
-
-    private
-     
-      def set_headers
-          if request.format.json?
-          response.content_type == "application/json+oembed"
-          elsif request.format.xml?
-           response.content_type == "text/xml+oembed" 
-          end
-          response.headers["Access-Control-Allow-Origin"] = "*"
+      if mainfile['file_type_tesim'].include? "3d"
+        embed_url = file_download_url(doc.id, mainfile.id, type: 'masterfile')
       end
 
+      raise DRI::Exceptions::NotFound if embed_url.nil?
 
+      resource_title = doc['title_tesim']
+
+      # Build up a JSON response with the required attributes
+      # See "2.3.4. Response parameters" at https://oembed.com/
+      @response = {
+        type: 'rich',
+        version: '1.0',
+        title: resource_title[0],
+        provider_name: 'DRI: Digital Repository of Ireland',
+        provider_url: 'https://repository.dri.ie/',
+
+        # not sure if this width and height is correct
+        width: 500,
+        height: 500,
+
+        # Embedding url
+
+        html: <<-HTML
+
+        <iframe src = "#{embed_url}" width="500" height="500">
+
+        </iframe>
+
+        HTML
+      }
+
+      respond_to do |format|
+        if @response
+          format.json { render json: @response }
+          format.xml  { render xml: @response }
+        else
+          format.all  { raise DRI::Exceptions::NotImplemented }
+        end
+      end
    end
 
-end	
+    private
+
+      def can_view?(doc)
+        (can?(:read, doc.id) && doc.read_master?) || can?(:edit, doc)
+      end
+
+      def set_headers
+        response.content_type = if request.format.json?
+                                  'application/json+oembed'
+                                elsif request.format.xml?
+                                  'text/xml+oembed'
+                                end
+        response.headers["Access-Control-Allow-Origin"] = "*"
+      end
+   end
+end
