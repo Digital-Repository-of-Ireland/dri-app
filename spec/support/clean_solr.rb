7a34004c5f1746aff615a033bf307e9fb3f6af0f
Stuart Kenny <skenny@tchpc.tcd.ie>
2022-09-07 12:04:08 +0100

    stop warnings about already initialised variable

--- spec/support/clean_solr.rb
+++ spec/support/clean_solr.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-SOLR_TEST_URL = "http://127.0.0.1:#{ENV['SOLR_TEST_PORT'] || 8983}/solr/test"
+SOLR_TEST_URL ||= "http://127.0.0.1:#{ENV['SOLR_TEST_PORT'] || 8983}/solr/test"
 RSpec.configure do |config|
   config.before(:all) do
     client = RSolr.connect(url: SOLR_TEST_URL)
