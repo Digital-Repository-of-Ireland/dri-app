b18b98bdec431be08e8e3252dd2b57be69511572
Stuart Kenny <kennystu@gmail.com>
2022-12-16 15:51:23 +0000

    fix for Pysch 4

--- config/initializers/doi_config.rb
+++ config/initializers/doi_config.rb
@@ -1,3 +1,3 @@
 config = Rails.root.join('config', 'doi.yml')
 
-DoiConfig = File.exists?(config) ? OpenStruct.new(YAML.load_file(config)[Rails.env]) : nil
+DoiConfig = File.exists?(config) ? OpenStruct.new(YAML.load_file(config, aliases: true)[Rails.env]) : nil
