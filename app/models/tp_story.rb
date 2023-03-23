f362daae34dc1d86e1ee40592364dceb72ca3790
Kathryn Cassidy <github@hellbunnie.org>
2023-03-23 13:59:44 +0000

    Fixing Transcribathon models and adding has_many relation from story to people and places for convenience

--- app/models/tp_story.rb
+++ app/models/tp_story.rb
@@ -1,3 +1,5 @@
 class TpStory < ApplicationRecord
-  has_many :story, class_name: 'TpItem', foreign_key:'story_id'
+  has_many :items, class_name: 'TpItem', foreign_key:'story_id'
+  has_many :people, class_name: 'TpPerson', through: 'items'
+  has_many :places, class_name: 'TpPlace', through: 'items'
 end
