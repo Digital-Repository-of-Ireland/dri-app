f6b3f478e3cf051e27f05709ca906707eaa86ce6
MuriloDiasTCD <mdias@tcd.ie>
2023-04-05 14:38:11 +0100

    - Fixed onWindowResize function; - Improve render process (lowering quality, removing point light and ignoring original skin); - Material modified to improve visibility of the object;
    Note: This code prints only a single 3D file per object.

--- app/assets/javascripts/three_js_viewer.js
+++ app/assets/javascripts/three_js_viewer.js
@@ -1,16 +1,10 @@
 $(document).ready(function () {
-	// Necessary for camera/plane rotation
-	var degree = Math.PI/180;
-	var  cameraTarget ,container;
 
-	container = document.createElement( 'div' );
+	var container = document.createElement( 'div' );
 	container.id = "ThreeDContainer";
 
-    parentDiv = document.getElementById("dri_threejs_view")
+    var parentDiv = document.getElementById("dri_threejs_view");
 	parentDiv.appendChild( container );
-
-    var width = parentDiv.parentNode.offsetWidth;
-    var height = parentDiv.parentNode.offsetHeight;
 
 	// Setup
 	var scene = new THREE.Scene();
@@ -22,28 +16,23 @@ $(document).ready(function () {
         5000
     );
 
-	camera.position.set( 3, 0.15, 3 );
-	cameraTarget = new THREE.Vector3( 0, - 0.25, 0 );
-
-	var renderer = new THREE.WebGLRenderer({antialias: true});
-	renderer.shadowMap.enabled = true;
-	renderer.setPixelRatio(window.devicePixelRatio);
+	var renderer = new THREE.WebGLRenderer({antialias: false});
+	renderer.shadowMap.enabled = false;
 	renderer.setSize(window.innerWidth/2, window.innerHeight/2);
-	renderer.outputEncoding = THREE.sRGBEncoding;
 	container.appendChild( renderer.domElement );
+    
+    var backgroundColour = '0xfffff0';
+	scene.background = new THREE.Color( backgroundColour );
 
-	scene.background = new THREE.Color( 0x72645b );
-
-	// Resize after viewport-size-change
+    // Resize after viewport-size-change
 	window.addEventListener( 'resize', onWindowResize, false);
 
     function onWindowResize() {
-		parentDiv = document.getElementById("dri_threejs_view")
-	    parentDiv.appendChild( container );
-
-		camera.aspect = width / height;
-		camera.updateProjectionMatrix();
-		renderer.setSize( width, height );
+        var width = parentDiv.clientWidth;
+        var height = parentDiv.clientHeight;
+        renderer.setSize(width, height);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
 	}
 
 	// Add controls
@@ -52,14 +41,12 @@ $(document).ready(function () {
     controls.target.set(0, 0, 0);
 
     var boxSize = new Array();
-    const absMaterial = new THREE.MeshPhongMaterial({ 
-        color: 0x1111111, 
+    const absMaterial = new THREE.MeshNormalMaterial({ 
         wireframe: false, 
         flatShading: false, 
         transparent: true,
     });
 
-    var isCustomMaterial = false;
 	var url = $('#dri_threejs_view').data('url');
     var extension;
 
@@ -76,7 +63,6 @@ $(document).ready(function () {
 
     switch(extension) {
         case 'stl':
-            isCustomMaterial = true;
             var stlLoader = new THREE.STLLoader();
             stlLoader.load( url, function ( geometry ) {
                 var mesh = new THREE.Mesh( geometry, absMaterial);
@@ -84,15 +70,14 @@ $(document).ready(function () {
                 guiInitializer(absMaterial, mesh);
             },
             (xhr) => {
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
             },
             (error) => {
-                console.log(error)
+                console.log(error);
             });
             break;
     
         case 'ply':
-            isCustomMaterial = true;
             const plyLoader = new THREE.PLYLoader();
             plyLoader.load( url, function ( ply ) {
                 ply.computeVertexNormals();
@@ -133,17 +118,19 @@ $(document).ready(function () {
                 materialConfig(fbx);
                 renderObject(fbx);
                 guiInitializer(absMaterial, fbx);
+                const spinner = document.getElementById('spinner');
+                spinner.parentNode.removeChild(spinner);
             },
             (xhr) => {
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+
             },
             (error) => {
-                console.log(error)
+                console.log(error);
             } );
             break;
     
         case 'obj':
-            isCustomMaterial = true;
             var objLoader = new THREE.OBJLoader( );
             objLoader.load( url, function (obj) {
                 materialConfig(obj);
@@ -166,10 +153,10 @@ $(document).ready(function () {
                 guiInitializer(absMaterial, dae.scene);
             },
             (xhr) => {
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
             },
             (error) => {
-                console.log(error)
+                console.log(error);
             } );
             
             break;
@@ -178,30 +165,14 @@ $(document).ready(function () {
             console.log("Unsupported file type: " + extension);
     }
 
-	function addShadowedLight( x, y, z, color, intensity ) {
-		var directionalLight = new THREE.DirectionalLight( color, intensity );
-		directionalLight.position.set( x, y, z );
-		scene.add( directionalLight );
-		directionalLight.castShadow = true;
-		var d = 1;
-		directionalLight.shadow.camera.left = - d;
-		directionalLight.shadow.camera.right = d;
-		directionalLight.shadow.camera.top = d;
-		directionalLight.shadow.camera.bottom = - d;
-		directionalLight.shadow.camera.near = 1;
-		directionalLight.shadow.camera.far = 4;
-		directionalLight.shadow.bias = - 0.002;
-	}
-
     function materialConfig(object){
         object.traverse( function(child){
             if(child.isMesh){
-                if(isCustomMaterial == true){
-                    child.material = absMaterial;
-                }
-                child.shadow = true;
-                child.castShadow = true;
-                child.receiveShadow = true;
+                child.material = absMaterial;
+                child.shadow = false;
+                child.castShadow = false;
+                child.receiveShadow = false;
+                child.frustumCulled = true;
             }
         });
     }
@@ -223,7 +194,6 @@ $(document).ready(function () {
     }
 
     function setCamera(objectSize){
-        scene.background = new THREE.Color(0xfffff0);
         camera.lookAt(new THREE.Vector3(0,0,0));
         let x,y,z;
     
@@ -240,20 +210,6 @@ $(document).ready(function () {
     
         camera.position.set(y, x, z+x+y);
     
-        // Add lights
-        var hemiLight = new THREE.HemisphereLight( 0x443333, 0x111122, 0.61 );
-        hemiLight.position.set( 0, 50, 0 );
-        scene.add( hemiLight );
-        addShadowedLight( 1, -1,  1, 0xffaa00, 1.35 );
-        addShadowedLight( 1,  1, -1, 0xffaa00, 1 );
-        const light1 = new THREE.PointLight();
-        const light2 = new THREE.PointLight();
-        light1.position.set(0, 0, z+x+y);
-        light2.position.set(0, 0, -z-x-y);
-        scene.add(light1);
-        scene.add(light2);
-        const ambientLight = new THREE.AmbientLight();
-        scene.add(ambientLight);
         cameraTarget = new THREE.Vector3( 0, 0, 0 );
     }
 
@@ -263,59 +219,73 @@ $(document).ready(function () {
         gui.domElement.id = "gui";
         parentDiv.appendChild(gui.domElement);
 
-        const options = { 
+        var options = { 
             wireframe: false,
             object: 0x1111111,
-            background: 0xd7d7d7,
-            lights: true,
-            material: true,
+            background: 0xfffff0,
+            lights: false,
+            material: false,
         }
 
         var refreshMaterial = {
             reset: function() {
-                location.reload(container.load);
+                material.wireframe = false;
+                material.flatShading = false;
+                material.opacity = 1.0;
+                material.needsUpdate = true;
+                scene.background = new THREE.Color(options.background);
+                resetGUI();
             }
         };
+
+        function resetGUI() {
+            // Get all the folders in GUI
+            const folders = gui.__folders;
+        
+            // Loop through each folder and reset properties
+            for (let folderName in folders) {
+                const folder = folders[folderName];
+                const controllers = folder.__controllers;
+        
+                // Loop through each controller in the folderand set default
+                for (let i = 0; i < controllers.length; i++) {
+                    const controller = controllers[i];
+                    controller.setValue(controller.initialValue);
+                }
+            }
+        }
         
         const materialFolder = gui.addFolder('Material Control');
         materialFolder.add(material, 'opacity', 0, 1, 0.01);
         materialFolder.add(material, 'wireframe').onChange(() => material.needsUpdate = true);
         materialFolder.add(material, 'flatShading').onChange(() => material.needsUpdate = true);
-        materialFolder.add(refreshMaterial, "reset").name("Reset Object");
-        if(isCustomMaterial == false) materialFolder.hide();
-
+        materialFolder.add(refreshMaterial, "reset").onChange(() => material.needsUpdate = true);
         const cameraFolder = gui.addFolder('Camera Control');
         cameraFolder.add(object.rotation, 'x', 0, Math.PI * 2);
         cameraFolder.add(object.rotation, 'y', 0, Math.PI * 2);
         cameraFolder.add(object.rotation, 'z', 0, Math.PI * 2);
     
         const colorFolder = gui.addFolder('Color Control');
-        colorFolder.addColor(options, 'object').onChange( col => {
-            isCustomMaterial = true;
-            materialFolder.show();
-            absMaterial.color = new THREE.Color(col);
-            if(['fbx', 'glb', 'gltf', 'obj', 'dae' ].includes(extension)) {
-                object.traverse( function(child){
-                    if(child.isMesh){
-                        materialConfig(child);
-                    }
-                });
-            }
-        });
         colorFolder.addColor(options, 'background').onChange( col => {
             scene.background = new THREE.Color(col);
         });
 
         
         gui.close();
-        onWindowResize();
+
     }
 
     //recursive function to update scene continuously
-    animate();
     function animate() {
-        requestAnimationFrame(animate)
-        controls.update()
-        renderer.render(scene, camera)
+        requestAnimationFrame(animate);
+        controls.update();
+        renderer.render(scene, camera);
     }
+    animate();
+
+    setTimeout(function() {
+        if (parentDiv.clientWidth > 0){
+        onWindowResize();
+        }
+      }, 2000);
 });
