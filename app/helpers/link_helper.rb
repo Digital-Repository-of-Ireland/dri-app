7b6f51d35304668573e86c14ca63da7ebdea79a4
Stuart Kenny <kennystu@gmail.com>
2023-10-13 17:39:43 +0100

    popovers and alignment fixes

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -2,9 +2,9 @@ module LinkHelper
   def review_link(text, url, popover_text)
     link_to text, url, {
       class: "dri_help_popover",
-      "data-content" => popover_text,
-      "data-trigger" => "hover",
-      "data-placement" => "left"
+      "data-bs-content" => popover_text,
+      "data-bs-trigger" => "hover",
+      "data-bs-placement" => "left"
     }
   end
 end
