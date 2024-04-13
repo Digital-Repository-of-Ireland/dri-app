52f8c9c48cb57fe964789d924740271418794f83
Stuart Kenny <kennystu@gmail.com>
2024-04-13 11:56:30 +0100

    create default allow metadata config for new collections

--- app/jobs/delete_collection_job.rb
+++ app/jobs/delete_collection_job.rb
@@ -7,5 +7,7 @@ class DeleteCollectionJob < IdBasedJob
   def run
     Rails.logger.info "Deleting all objects in #{object.alternate_id}"
     object.destroy
+
+    CollectionConfig.find_by(collection_id: object.alternate_id).destroy if CollectionConfig.exists?(collection_id: object.alternate_id)
   end
 end
